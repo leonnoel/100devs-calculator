614ea574c51217fa08764bc445f7f57bcfd17a16
Yash Parekh <yash20010@gmail.com>
2021-01-26 13:58:46 -0700

    Updated classed in HTML class='num' for the numbers and 'operator' for the operation buttons' Completed main.js Used 'Web Dev Simplified' as a reference (YouTube)

--- js/main.js
+++ js/main.js
@@ -1,34 +1,110 @@
-let butt = Array.from(document.querySelectorAll('button'))
+class Calculator {
+  constructor(chainTextElement, resultTextElement){
+    this.chainTextElement = chainTextElement
+    this.resultTextElement = resultTextElement
+    this.chain = ''
+    this.result = ''
+    this.operation = undefined
+  }
+  appendNumber(number){
+    if (number === '.' && this.result.includes('.')) return
+    this.result = this.result.toString() + number.toString()
+  }
 
-butt.forEach(but => but.addEventListener('click',type))
+  chooseOperation(operation){
+    if(this.result === '') return
+    if(this.chain !== ''){
+      this.compute()
+    }
+    this.operation = operation
+    this.chain = this.result
+    this.result = ''
 
-function type(b){
-  if(b.toElement.innerText !== '='){
-    let op = document.querySelector('#chain')
-    op.innerText += b.toElement.innerText
-  }else if(b.toElement.innerText === '='){
-    document.querySelector('#result').innerText = powerCalc.calculate(document.querySelector('#chain').innerText)
-    document.querySelector('#chain').innerText = document.querySelector('#result').innerText
+  }
+
+  compute(){
+    let computation
+    const cha = parseFloat(this.chain)
+    const res = parseFloat(this.result)
+    if(isNaN(cha) || isNaN(res)) return
+    switch (this.operation){
+      case '+':
+        computation = cha + res
+        break
+      case 'x':
+        computation = cha * res
+        break
+      case '-':
+        computation = cha - res
+        break
+      case '/':
+        computation = cha / res
+        break
+      default:
+        return
+    }
+    if(!Number.isInteger(computation)){
+      this.result = computation.toFixed(2)
+    }else{
+      this.result = computation
+    }
+    this.operation = undefined
+    this.chain = ''
+  }
+
+  //getDisplayNumber was the hardest to understand
+  getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if (isNaN(integerDigits)) {
+      integerDisplay = ''
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } else {
+      return integerDisplay
+    }
+  }
+
+  updateDisplay(){
+    this.resultTextElement.innerText = this.getDisplayNumber(this.result)
+    if(this.operation != null){
+      this.chainTextElement.innerText = `${this.getDisplayNumber(this.chain)} ${this.operation}`
+    }else{
+      this.chainTextElement.innerText = ''
+    }
+
   }
 }
 
-function Calculator() {
 
-  this.methods = {
-    "-": (a, b) => a - b,
-    "+": (a, b) => a + b,
-    "/": (a, b) => a / b,
-    "x": (a, b) => a * b
-  };
+const numberButtons = document.querySelectorAll('.num')
+const operationButtons = document.querySelectorAll('.operator')
+const equalsButton = document.querySelector('.equals')
+const chainTextElement = document.querySelector('#chain')
+const resultTextElement = document.querySelector('#result')
 
-  this.calculate = function(str) {
+const calculator = new Calculator(chainTextElement,resultTextElement)
 
-    let split = str.split(''),
-      a = +split[0],
-      op = split[1],
-      b = +split[2];
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
 
-    return this.methods[op](a, b);
-  };
-}
-let powerCalc = new Calculator
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+equalsButton.addEventListener('click' , button => {
+  calculator.compute()
+  calculator.updateDisplay()
+})
