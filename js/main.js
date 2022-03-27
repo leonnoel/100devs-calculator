2a280dc52427eb3f7a0b4040c812bba7748df4ce
Sach <sss@apple.com>
2022-03-27 14:50:06 +0100

    layed out architecture of Calculator constructor

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function Calculator() {
+  let enteredValues = [];
+  let currentValue = 0;
+
+  this.divide = function () {
+    return this;
+  };
+
+  this.mutliply = function () {
+    return this;
+  };
+
+  this.add = function () {
+    return this;
+  };
+
+  this.subtract = function () {
+    return this;
+  };
+
+  this.enterValue = function () {
+    // if "=" selected complete computation
+    // if operand add previous pending value to enteredValues, add operand after
+    // if number store number in string
+  };
+}
