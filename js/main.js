76be493a4588eb543dbdb985cab0d5d8fd77f19f
Sach <sss@apple.com>
2022-03-27 15:25:22 +0100

    Implemented Calculator constructor

--- js/main.js
+++ js/main.js
@@ -1,26 +1,64 @@
 function Calculator() {
   let enteredValues = [];
-  let currentValue = 0;
+  let currentValue = "0";
+  const operators = ["/", "*", "+", "-"];
 
-  this.divide = function () {
-    return this;
+  let calcs = {
+    "/": (a, b) => a / b,
+    "*": (a, b) => a * b,
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
   };
 
-  this.mutliply = function () {
-    return this;
+  this.calculate = function () {
+    for (let operator of operators) {
+      while (enteredValues.indexOf(operator) >= 0) {
+        let actionIndex = enteredValues.indexOf(operator);
+        let action = enteredValues[actionIndex];
+        let result = calcs[action](
+          enteredValues[actionIndex - 1],
+          enteredValues[actionIndex + 1]
+        );
+        enteredValues[actionIndex - 1] = result;
+        enteredValues.splice(actionIndex, 2);
+      }
+    }
+    let answer = enteredValues[0];
+    currentValue = answer;
+    console.log(answer);
   };
 
-  this.add = function () {
-    return this;
+  this.enterValue = function (value) {
+    if (value === "=") {
+      // if "=" selected complete computation
+      enteredValues.push(parseFloat(currentValue));
+      // enteredValues.push(value);
+      this.calculate();
+    } else if (operators.includes(value)) {
+      // if operand add previous pending value to enteredValues, add operand after
+      enteredValues.push(parseFloat(currentValue));
+      enteredValues.push(value);
+      currentValue = "0";
+    } else {
+      // if number store number in string
+      currentValue += value;
+    }
   };
 
-  this.subtract = function () {
-    return this;
+  this.reset = function () {
+    enteredValues = [];
+    currentValue = "0";
   };
+}
 
-  this.enterValue = function () {
-    // if "=" selected complete computation
-    // if operand add previous pending value to enteredValues, add operand after
-    // if number store number in string
-  };
+let calc = new Calculator();
+
+for (let char of "9/3*4+3-2=") {
+  calc.enterValue(char); // returns 13
+}
+
+calc.reset();
+
+for (let char of "9-3*4+3/2=") {
+  calc.enterValue(char); // returns -4.5
 }
