5a06c588e6cfdd5f497935e10a427fd24801ebbc
Alex Pan <alexanderypan@gmail.com>
2022-03-27 23:36:04 -0400

    Add tests, clear equation , refactor, update known issues
    - Add methods to test for valid inputs
    - Add feature to clear equation on double '=' press
    - refactor several functions / add comments
    - Update known issues list

--- js/main.js
+++ js/main.js
@@ -4,21 +4,17 @@
     // calc.addToEquation() is the main function that wraps around most other methods in the object.
         // addToEquation checks in the input and, if valid, either adds the input to the equation or evaluates the equation.
         // Evaluates only 1 operation at a time (ie., [term1] [operator] [term2]). If there is a complete operation and  another operator is clicked, the operation will evaluate before the new operator is added (I was too lazy to deal with order of operations).
-    // Supports ~40 characters afterwhich they go offscreen.
 
 // Improvements
     // Features
         // Handle addition of new operators such as **, ()
         // Pressing '=' twice clears the equation
 
-    // testing / edge cases
-        // Operator is valid
-        // Each equation has only 1 operator
-        // Each term has max 1 decimal place
+    // Known issues
+        // Supports ~40 characters afterwhich they go offscreen.
+        // Once values become too large for `number` they are converted to Scientific notation and calculations break
+        // Leading negative sign breaks calculations
 
-    // error handling
-        // if equation is invalid throw error
-        // If using 2 operators in a row, overwrite the first operator
 
 
 document.addEventListener('click', buttonClick)
@@ -26,6 +22,7 @@ document.addEventListener('click', buttonClick)
 
 function makeCalc(equation) {
     this.equation = equation;
+    this.equalsCounter = 0;
     this.operations = {
         '+' : (a, b) => a + b,
         '-' : (a, b) => a - b,
@@ -33,16 +30,21 @@ function makeCalc(equation) {
         'x' : (a, b) => a * b,
         "/" : (a, b) => a / b,
     }
-    
-    this.addToEquation = function(input) {  // Called whenever a button other than '=' is pressed
-        if ((input in this.operations || input == '=') && this.isCompleteEquation()) {    // If complete equation, evaluate before adding.
-            this.equation = this.calculate()
-        }
 
-        if (input !== '=') {
-            this.equation = this.equation + input
+    this.addToEquation = function(input) {
+        // If invalid button is pressed, ignore it.
+        if (!this.isValidInput(input)) return this.equation  
+
+        // Check if equals has been pressed x2 in a row. If so clear equation.
+        if (this.isDoubleEquals(input)) return this.equation  
+
+        // If input is an operator or '=' and the current equation is a complete equation, 
+        // evaluate operation before concatenating next operator
+        if ((this.isValidOperator(input) || input === '=') && this.isCompleteEquation()) { 
+            this.equation = this.calculate()
+            this.equation = (input === '=') ? this.equation : this.equation + input;
+            return this.equation
         }
-        return this.equation
     }
 
     this.splitEquation = function() {
@@ -51,25 +53,70 @@ function makeCalc(equation) {
         return eq
     }
 
-    this.calculate = function() { // Called when 'equals button' is pressed 
+    this.calculate = function() {
         eq = this.splitEquation();
-        return String(this.operations[eq[1]](BigInt(eq[0]), BigInt(eq[2])));
+        return String(this.operations[eq[1]](+eq[0], +eq[2]));
     }
 
     this.isCompleteEquation = function() {
-        const eq = this.splitEquation();
-         // Cases handled: 
+        eq = this.splitEquation();
+        // A complete equation is: [term1] [operator] [term2]
+        // Cases handled: 
             // Equation has 1 operator.
             // The first term is either empty or a valid number (treated as 0 if omitted)
             // The second term is non-empty and a valid number
             // The operator is valid.
-        if (eq.length != 3 || isNaN(+eq[0]) || eq[2] == '' || isNaN(+eq[2]) || !'+-*/x'.includes(eq[1])) {
+        if (eq.length != 3 
+            || isNaN(+eq[0]) 
+            || eq[2] == '' 
+            || isNaN(+eq[2]) 
+            || !'+-*/x'.includes(eq[1])) {
             return false // not a valid equation
         } else {
          return true 
         }
     }
+
+    this.isDoubleEquals = function(input) {
+        if (input === '=') {
+            this.equalsCounter += 1
+        } else {
+            this.equalsCounter = 0
+        }
+        if (this.equalsCounter >= 2) {
+            this.equalsCounter = 0;
+            this.equation = ''
+            return true
+        }
+        return false
+    }
+
+    this.isValidOperator = function(input) {
+        return input in this.operations
+    }
+
+    this.endsWithOperator = function() {
+        return this.equation.slice(-1) in this.operations
+    }
+
+    this.hasDecimal = function() {
+        eq = this.splitEquation()
+        for (i=0; i < eq.length; i++) {
+            let splits = eq[i].split('.').length
+            if(splits > 1) return false
+        }
+        return true
+    }
+
+    this.isValidInput = function(input) {
+        const validOperator = this.isValidOperator(input);
+        if (this.endsWithOperator() && validOperator) return false    // check if 2 operators in a row
+        if (! this.hasDecimal() && input ==='.') return false     // Check if term has a decimal & is adding a new one. No more than 1 per term.
+        return true
+    }
+
 }
+
 
 const calc = new makeCalc('')
 
