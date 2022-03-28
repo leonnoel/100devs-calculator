c7dd28500c0780ae5d150aa231323b5c5b0f4f7b
NatashaPettinger <natasha.pettinger@gmail.com>
2022-03-28 16:37:20 -0600

    first draft

---
+++ js/main.js
@@ -0,0 +1,75 @@
+//create two variables, a & b, that store the numerical values that are input into the calculator & third variable total.
+
+let a = 0
+let b = 0
+let total
+
+//create a variable that stores whether numbers should be input into storage a or b
+//create a variable that stores what operation was selected
+let op;
+let ph = 1;
+
+const numbers = document.querySelectorAll('.number')
+Array.from(numbers).forEach(number => number.addEventListener('click', findNumber))
+
+//Store clicked number in a variable & then run storeNumber function.
+function findNumber (click) {
+    let num = click.target.innerText;
+    storeNumber(num, ph);
+}
+
+function storeNumber (number, pM) {
+   switch (pM) {
+        case 1: 
+            a = a + number;
+            displayNumber(a);
+            break;
+        case 2:
+            b = b + number;
+            displayNumber(b);
+            break;
+   }
+}
+
+function displayNumber (x) {
+    document.querySelector('#answerBox').innerText = x;
+}
+function displayOp (x) {
+    document.querySelector('#opHolder').innerText = x;
+}
+
+//operator functions
+const ops = document.querySelectorAll('.op')
+Array.from(ops).forEach(op => op.addEventListener('click', findOperation))
+
+function findOperation(click) {
+    op = click.target.innerText;
+    ph = 2;
+    displayOp(op);
+}
+
+document.querySelector('.equals').addEventListener('click', result)
+function result() {
+    let total;
+    switch (op) {
+        case '/':
+            total = Number(a)/Number(b);
+            displayNumber(total);
+            break;
+        case 'X':
+            total = Number(a)*Number(b);
+            displayNumber(total);
+            break;
+        case '+':
+            total = Number(a)+Number(b);
+            displayNumber(total);
+            break;
+        case '-':
+            total = Number(a) - Number(b);
+            displayNumber(total);
+            break;
+    }
+    a = total;
+    b = 0;
+    ph = 1;
+}
