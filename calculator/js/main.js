89219de5cf6c4111840827a4d552b911ea4b157f
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-03-29 23:47:52 +0300

    create the structure and the shape of the calculator

---
+++ calculator/js/main.js
@@ -0,0 +1,15 @@
+// listen for clicks
+// add event listener for each btn
+// the function for each btn is check for the btn inner text
+// if the inner is nums
+// fire another function that update the screen text add numbers one beside the next and check if there is a dot(.) that it not allowed two or more dots in the same text
+// if the inner is operator
+// save the number
+// activate the operator
+// listern for the btns click
+// repeat the function to update the screen
+// if the btn inner is =
+// the fire the calculatrion method
+// it result the (first num)(operator)(second num) and return the resutl
+// fire the update the screen function to show the result
+// round the number to lets say 5 digits with the .toFixed(5)
