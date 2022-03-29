b51cd3b02fdbb07feb6c3891633607904f395aed
efs0-cod3 <eflearnng@gmail.com>
2022-03-29 08:42:06 -0400

    js starting

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+// consts
+const display = document.getElementById('placeToPutResult')
+const numberKeys = document.querySelectorAll('[data-number]')
+const operatorKeys = document.querySelectorAll('[data-operator]')
+const equalKey = document.querySelector('.eq')
+const actionAc = document.querySelector('.ac')
+const actionDel = document.querySelector('.del')
+const actionDot = document.querySelector('.dot')
