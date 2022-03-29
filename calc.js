df1927f50a044a5407a1d39a620aa6377825d0b7
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2022-03-28 22:29:35 -0500

    - Add backspace/undo button. - Refactor method names for better description. - Add explanatory comments where reason for decisions might be unclear.

--- calc.js
+++ calc.js
@@ -14,11 +14,12 @@ class Calculator {
             number.addEventListener('click', () => this.pressButton(number.innerHTML))
         })
         document.querySelector('.clear').addEventListener('click', () => this.clearDisplay())
+        document.querySelector('.backspace').addEventListener('click', () => this.backspace())
         document.querySelector('.add').addEventListener('click', () => this.add())
         document.querySelector('.subtract').addEventListener('click', () => this.subtract())
         document.querySelector('.multiply').addEventListener('click', () => this.multiply())
         document.querySelector('.divide').addEventListener('click', () => this.divide())
-        document.querySelector('.equal').addEventListener('click', () => this.equal())
+        document.querySelector('.equal').addEventListener('click', () => this.evaluate())
         document.querySelector('.decimal').addEventListener('click', () => this.pressButton('.'))
     }
     // ----------------------------
@@ -36,12 +37,21 @@ class Calculator {
         this.display.classList.remove('error')
         this.display.classList.remove('too-long')
     }
-    clearIfErrorText() {
+    clearErrorText() {
         if (this.display.innerHTML.includes(this.errorMessage))
             this.clearDisplay()
     }
+    backspace() {
+        this.clearErrorText()
+        // Check if display is zeroed out first, so we don't also delete the default 0.
+        this.display.innerHTML.length > 1 ?
+            this.display.innerHTML = this.display.innerHTML.slice(0, -1) :
+            this.clearDisplay()
+        flashDisplay(this.display, 'cancel')
+    }
     // ----------------------------
     pressButton(number) {
+        // Check for zeroed out display, so we don't have leading zeros when adding numbers.
         this.display.innerHTML = this.display.innerHTML === '0' ? number : this.display.innerHTML + number
     }
     add() {
@@ -56,13 +66,15 @@ class Calculator {
     divide() {
         this.display.innerHTML += '/'
     }
-    equal() {
-        this.clearIfErrorText()
+    evaluate() {
+        this.clearErrorText()
         try {
             flashDisplay(this.display, 'flash')
             // Eval is a bad idea, but it's fine for this project!
             this.display.innerHTML = eval(this.display.innerHTML)
         } catch (SyntaxError) {
+            // Since the display line is evaluated with eval(),
+            // we should give feedback if the input was completely invalid. Eg: "2+4++".
             flashDisplay(this.display, 'cancel')
             this.display.classList.add('error')
             this.display.innerHTML = this.errorMessage
