82741dc5eac14ea4c82e98ded97b44480777e284
ruvsphysics <jihosk1995@live.com>
2022-03-29 18:00:44 -0700

    in progress

--- js/main.js
+++ js/main.js
@@ -1,35 +1,79 @@
 class Calculator {
     constructor() {
-        this.currentValue = 0
-        this.nextValue = null
+        this.val = "0"
+        this.op = ""
+        this.buffer = ""
+        this.displayBuffer = false
     }
+    display() {
+        if (!this.displayBuffer) {document.getElementById("display").innerText = this.val} 
+        else {document.getElementById("display").innerText = this.buffer}
 
-    onClick(click) {
-        const numClicked = num => {
-            console.log('numClicked!', num)
+        const style = {
+            backgroundColor: "#00acee"
         }
-        //onClick event
-        console.log(click.target.id)
-        if (/[0-9]/.test(click.target.id)) {
-            //Number is clicked
-            console.log('num is clicked')
-            numClicked(click.target.id)
-        } else if (/[-+*\/]/.test(click.target.id)) {
-            //Op is clicked
-            console.log('op is clicked')
-        } else if (/=/.test(click.target.id)) {
-            //= is clicked
-            console.log('= is clicked')
-        } else if (/\./) {
-            //. is clicked
-            console.log('. is clicked')
+        if (this.op) {
+            console.log(document.getElementById(`${this.op}`).style = {color: "#00acee"})
+            
         }
+    }
+    numClicked(num) {
+        if (this.buffer) {
+            // buffer not empty
+            this.displayBuffer = true
+            if (!/\./.test(this.buffer) && this.buffer==0 && num!=".") {this.setThis('buffer', num)} 
+            else if (num!=".") {this.setThis('buffer', num, true)}
+            else if (num=="." && !/\./.test(this.buffer)) {this.setThis('buffer', num, true)}
+        } else {
+            // buffer empty
+            this.displayBuffer = false
+            if (this.val==0 && num!=".") {this.setThis('val', num)} 
+            else if (num != ".") {this.setThis('val', num, true)}
+            else if (num == "." && !/\./.test(this.val)) {this.setThis('val', num, true)}
+        } 
+    }
+    opClicked(op) {
+        if (!this.buffer) {this.op = op} 
+        else if (this.op) {
+            this.equal()
+            this.op = op
+        }
+    }
+    equal() {
+        if (this.op && this.buffer) {
+            this.displayBuffer = false
+            this.val = (this.calculate(this.val, this.buffer)).toString()
+            this.buffer = ""
+            this.op = ""
+        }
+    }
+    calculate(val1, val2) {
+        if (this.op == "+") {return Number(val1)+Number(val2)}
+        else if (this.op == "-") {return Number(val1)-Number(val2)}
+        else if (this.op == "*") {return Number(val1)*Number(val2)}
+        else if (this.op == "/") {return Number(val1)/Number(val2)}
+    }
+    setThis(prop, val, concat=false) {
+        if (concat) {this[prop] += val}
+        else {this[prop] = val}
+    }
+    
+}
+function calculatorOnClick(click) {
+    if (/[0-9\.]/.test(click.target.id)) {
+        calculator.numClicked(click.target.id)
+        calculator.display()
+    } else if (/[-+*\/]/.test(click.target.id)) {
+        calculator.opClicked(click.target.id)
+        calculator.display()
+    } else if (/=/.test(click.target.id)) {
+        calculator.equal()
+        calculator.display()
     }
 }
-
 const calculator = new Calculator()
 const buttons = document.querySelectorAll("button")
 Array.from(buttons).forEach(b => {
     b.id = b.innerText == "×"? "*" : b.innerText
-    b.onclick = calculator.onClick
+    b.onclick = calculatorOnClick
 })
