b3f1c9143fe7110b07dacdae3624797f9538b115
Daniel <dan9221@msn.com>
2022-03-30 15:09:47 -0700

    V1, working fairly well, needs some formatting

--- js/main.js
+++ js/main.js
@@ -20,21 +20,127 @@ class Calculator {
 }
 
 let calc = new Calculator();
+let resultInWindow = false;
 let windowText = document.querySelector('.window span');
 document.addEventListener('keydown', keyPress);
 
+//function to check if an operator already exists in window
+function includesOperator(str) {
+    return str.includes('+') || str.includes('-') || str.includes('*') || str.includes('/');
+}
+
+// function to handle key press events
 function keyPress(key) {
-    if ( (+key.key >= 0 && +key.key <= 9) || key.key === '.') {
+    if( resultInWindow && +key.key >= 0 && +key.key <= 9)  {
+        windowText.innerText = '';
+        resultInWindow = false;
+    }
+
+    // if key pressed is 0-9 or a decimal point concat key to string
+    if ( (+key.key >= 0 && +key.key <= 9)) {
         windowText.innerText += key.key;
     } else {
         switch(key.key) {
+            case '.':
+                // if there isn't already a point concat one into window
+                if(!windowText.innerText.includes('.') || includesOperator(windowText.innerText)) {
+                        windowText.innerText += '.';
+                }
+                break;
             case 'Backspace':
                 let digits = windowText.innerText.split('');
                 digits.pop();
                 windowText.innerText = digits.join('');
                 break;
             case '+':
-                windowText.innerText = +2 + +2;
+                // if window isn't blank and doesn't already contain + sign then add + sign
+                if(windowText.innerText !== '' && !includesOperator(windowText.innerText)) {
+                    windowText.innerText += '+';
+                    resultInWindow = false;
+                }
+                break;
+            case '-':
+                // if window isn't blank and doesn't already contain + sign then add + sign
+                if(windowText.innerText !== '' && !includesOperator(windowText.innerText)) {
+                    windowText.innerText += '-';
+                    resultInWindow = false;
+                }
+                break;
+            case '*':
+                // if window isn't blank and doesn't already contain + sign then add + sign
+                if(windowText.innerText !== '' && !includesOperator(windowText.innerText)) {
+                    windowText.innerText += '*';
+                    resultInWindow = false;
+                }
+                break;
+            case '/':
+                // if window isn't blank and doesn't already contain + sign then add + sign
+                if(windowText.innerText !== '' && !includesOperator(windowText.innerText)) {
+                    windowText.innerText += '/';
+                    resultInWindow = false;
+                }
+                break;
+            case 'Enter':
+                let text = windowText.innerText;
+                let operands = [];
+
+                // if addition add and show result
+                if(text.includes('+')) {
+                    operands = text.split('+');
+                    if(operands[1] !== '') {
+                        let sum = calc.add(+operands[0], +operands[1]);
+                        windowText.innerText = sum;
+
+                        //set result in window bool to true
+                        //(so window can be cleared above when user types)
+                        resultInWindow = true;
+                    }  
+                }
+
+                // if subtraction subtract and show result
+                if(text.includes('-')) {
+                    operands = text.split('-');
+                    if(operands[1] !== '') {
+                        let difference = calc.subtract(+operands[0], +operands[1]);
+                        windowText.innerText = difference;
+
+                        //set result in window bool to true
+                        //(so window can be cleared above when user types)
+                        resultInWindow = true;
+                    }  
+                }
+
+
+
+                // if multiplication multiply and show result
+                if(text.includes('*')) {
+                    operands = text.split('*');
+                    if(operands[1] !== '') {
+                        let product = calc.multiply(+operands[0], +operands[1]);
+                        windowText.innerText = product;
+
+                        //set result in window bool to true
+                        //(so window can be cleared above when user types)
+                        resultInWindow = true;
+                    }  
+                }
+
+
+                // if division divide and show result
+                if(text.includes('/')) {
+                    operands = text.split('/');
+                    if(operands[1] !== '') {
+                        let quotient = calc.divide(+operands[0], +operands[1]);
+                        windowText.innerText = quotient;
+
+                        //set result in window bool to true
+                        //(so window can be cleared above when user types)
+                        resultInWindow = true;
+                    }  
+                }
+
+
+
                 break;
         }
     }
