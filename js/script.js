b30e2efe64238680be320800bd6a19324eacd836
jesus.doza <jesus.doza@gmail.com>
2022-03-30 10:17:29 -0500

    fix css to display numbers better with divs inside li, add classes and id to buttons, start javascript psuedo code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,40 @@
 let buttons=document.querySelectorAll('#buttons li')
 
+let answer = 0; //after math display this answer
+let display = ''; // keeps adding to screen as you press buttons
+let num1=0;// first
+let num2=0; 
+
+//add event listener to all buttons to put text to screen section
+buttons.forEach((e)=>{
+    e.addEventListener('click',displayThis)
+})
 
 
 
 
 
+// when button is pressed click
 
-// when button is pressed
+// add to h2 whenever button is clicked
 
-// add to h2
+//place number inside variable for first number num1
 
-//place number inside variable aslong its a number
+//when math symbol is pressed varible for operation is set operator
 
-//when math symbole is pressed change to another variable to input numbers
+//second variable will recieve numbers pressed num2
 
+//change to another variable to input numbers
+
+//eqauls is pushed then math is done and set into answer
+
+//answer is displayed and set to num 1 maybe?
+
+//repeat again to keep doing math on answer if desired
+
+
+
+//displayThis function to print to screen section on calculator  ******
+function displayThis (str){
+    //keep adding things to display as you press buttons
+}
