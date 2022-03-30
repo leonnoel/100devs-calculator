c21c93e0e244c377b1eea2b75f3b0f4e1cf31488
kgni <admin@mdia.dk>
2022-03-30 18:11:46 +0200

    change calculator window to be input field instead, with a placeholder of 0

---
+++ main.js
@@ -0,0 +1,7 @@
+// Have an input window, where all values and math operations are going to show up. When the equal sign is pressed, all the math operations that has been made is going to be evaluated and put into a sum variable, that will display
+
+function Calculator() {
+	this.sum = 0;
+	this.currentValue = 0;
+	this.input = 0;
+}
