27fdd11be570435bd8aadb4e1c4c3f1e8fd476dd
jesus.doza <jesus.doza@gmail.com>
2022-03-29 21:03:08 -0500

    add files with basic code to start working on calculator javascript

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let buttons=document.querySelectorAll('#buttons li')
+
+
+
+
+
+
+
+// when button is pressed
+
+// add to h2
+
+//place number inside variable aslong its a number
+
+//when math symbole is pressed change to another variable to input numbers
+
