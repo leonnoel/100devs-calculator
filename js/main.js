6e7cc2ff1b008e433edc93a6be9a18f55ac800fa
Daniel <dan9221@msn.com>
2022-03-30 10:45:54 -0700

    proof of concept working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+let windowText = document.querySelector('.window span');
+// windowText.innerText = 'test';
 
+document.addEventListener('keypress', keyPress);
+
+function keyPress(key) {
+    switch(key.key) {
+        case '0':
+            windowText.innerText += '0';
+            break;
+    }
+  }
