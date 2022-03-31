730a2c098504e946e9b7ddbad5a9d20c35a73e64
EricGallegos <46502883+EricGallegos@users.noreply.github.com>
2022-03-31 11:20:12 -0600

    removed references to the calc object literal in the calculator constructor methods

--- js/main.js
+++ js/main.js
@@ -9,67 +9,70 @@ function Calc(){
   this.buffer = 0;
   this.operator = '+'; // default operator so inital value is added to buffer
 
-////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 //update display method
-  this.refresh = function(){
-    if(this.value == ''){
+  this.refresh = function(obj, section){
+    if(this.value == ''){ // if value is empty string, show 0 on display
       calc.display.innerHTML = '0';
     }
-    else
-      calc.display.innerHTML = calc.value;
+    else calc.display.innerHTML = calc.value; // otherwise show string in value;
   }
-///////////////////////////////////////////////
-//receive input method
-  this.input = function(){
-    if(this.classList.contains('number')){
-      calc.value += this.innerHTML;
-      console.log(calc.value);
-      calc.refresh();
-    }
-    else if(this.classList.contains('operator')){
-      calc.eval(this.innerHTML);
-      calc.refresh();
-    }
-  }
-  /////////////////////////////////////////////
-  //evaluation method
-  this.eval = function(operator){
-    if(calc.operator == '+') { //addition
-      calc.buffer += +calc.value;
-    }
-    else if(calc.operator == '-') {//subtraction
-      calc.buffer -= +calc.value;
-    }
-    else if(calc.operator == 'x') {//multiplication
-      calc.buffer *= +calc.value;
-    }
-    else if(calc.operator == '/') {//division
-      calc.buffer /= +calc.value;
-    }
-    calc.operator = operator; //set operator to be used next
 
-    if(operator == '='){  //
-      calc.value = calc.buffer.toString(); // pull value from the buffer
-      calc.buffer = 0;
-      calc.operator = '+'; //reset operator back to default
+////////////////////////////////////////////////////////////////////////////////////////
+//receive input method
+  this.input = function(obj, section){
+    if(section.classList.contains('number')){ // if number add it to display string
+      obj.value += section.innerHTML;
+      obj.refresh(obj, section);
+    }
+    else if(section.classList.contains('operator')){ // if operator perform evaluation
+      obj.eval(section.innerHTML, obj);
+      obj.refresh(obj, section);
+    }
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////////
+  //evaluation method
+  this.eval = function(operator, obj){
+    if(obj.operator == '+') { //addition
+      obj.buffer += +obj.value;
+    }
+    else if(obj.operator == '-') {//subtraction
+      obj.buffer -= +obj.value;
+    }
+    else if(obj.operator == 'x') {//multiplication
+      obj.buffer *= +obj.value;
+    }
+    else if(obj.operator == '/') {//division
+      obj.buffer /= +obj.value;
+    }
+    obj.operator = operator; //set operator to be used next
+
+    if(operator == '='){  //In this case display the buffer and reset values
+      obj.value = obj.buffer.toString(); // pull value from the buffer
+      obj.buffer = 0;
+      obj.operator = '+'; //reset operator back to default
       return; // leave function without resetting calc.value so the display shows the answer
     }
-    calc.value = ''; //resets display to 0 whenever an operator is used except =
+    obj.value = ''; //resets display to 0 whenever an operator is used except =
   }
-  /////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////////////////
   //reset calcultor to default values
-  this.reset = function(){
-      calc.value = '';
-      calc.buffer = 0;
-      calc.operator = '+';
-      calc.refresh();
+  this.reset = function(obj, section){
+      obj.value = '';
+      obj.buffer = 0;
+      obj.operator = '+';
+      obj.refresh();
   }
 }
-
+//////////////////////////////////////////////////////////////////////////////////////
+//main
 const calc = new Calc(); // create a new calc object;
-calc.refresh();          // initialise the display
+const display = document.querySelector('.display');
+calc.refresh(calc, display);          // initialise the display
 
 document.querySelectorAll('.button').forEach(button =>
-    button.addEventListener('click', calc.input));
+    button.addEventListener('click', function(){calc.input(calc, button)}));
 
-document.querySelector('.display').addEventListener('click', calc.reset)
+document.querySelector('.display').addEventListener('click', function(){calc.reset(calc, display)});
