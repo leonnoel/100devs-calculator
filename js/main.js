9346e3e15336f971a32d48c171c09cd034cdd6a7
ZRTAssassin <36104436+ZRTAssassin@users.noreply.github.com>
2022-03-30 19:46:59 -0500

    Basic functionality done

--- js/main.js
+++ js/main.js
@@ -1,16 +1,71 @@
-let spans = document.querySelectorAll("span");
-for (const button of spans) {
-    button.addEventListener('click', Hi);
-}
+function Calculator() {
+    let a = 0;
+    let currentNumber = '';
+    let currentOperation = '';
 
-let answer = document.querySelector("#answer");
-function Hi(){
-    console.log("HEY!");
-    answer.innerHTML = "0000";
-}
 
-function Calculator(){
+    this.equals = function (a, b, operator) {
+        console.log(`${a} ${operator} ${b}`);
+    }
+    /*return a + b;
+    return a - b;
+    return a * b;
+    return a / b;
+    */
+    this.addNumberToCurrent = function () {
+        //console.log(this.innerHTML);
+        //console.log(calc.currentNumber);
+        currentNumber = currentNumber + String(this.innerHTML);
+        console.log(currentNumber);
+        answer.innerHTML = currentNumber;
+    }
+    this.addOperand = function () {
+        //console.log(`${this.innerHTML}`);
+        currentOperation = this.innerHTML;
+        a = currentNumber;
+        currentNumber = '';
+        console.log(`${this.innerHTML}`);
+    }
+    this.compute = function () {
+        a = Number(a);
+        currentNumber = Number(currentNumber);
+        let result = 0;
+        if (currentOperation === "+") {
+            result = a + currentNumber;
+        } else if (currentOperation === "-") {
+            result = a - currentNumber;
+        } else if (currentOperation === "*") {
+            result = a * currentNumber;
+        } else if (currentOperation === "/") {
+            result = a / currentNumber;
+        } else {
+            answer.innerHTML = "Invalid";
+        }
+        answer.innerHTML = result;
+        a = 0;
+        currentNumber = '';
+        currentOperation = '';
+        a = result;
+        
+    }
 
+    let nums = document.querySelectorAll(".number");
+    for (let num of nums) {
+        num.addEventListener('click', this.addNumberToCurrent);
+    }
+
+    let operands = document.querySelectorAll(".operand");
+    for (let operand of operands) {
+        operand.addEventListener('click', this.addOperand);
+    }
+    let compute = document.querySelector(".compute");
+    compute.addEventListener('click', this.compute)
+
+    let answer = document.querySelector("#answer");
 }
 
 let calc = new Calculator();
+
+
+
+
