4bb8d94d9c54a8414b40cc5811a5b5a822e3136e
ned-park <87483870+ned-park@users.noreply.github.com>
2022-03-30 21:35:38 -0400

    Refactored code following OOP principles

--- js/main.js
+++ js/main.js
@@ -1,103 +1,120 @@
-let equation = []
-let reset = true
+class Button {
+  constructor(name, value) {
+    this.name = name
+    this.value = value
+  }
 
-function Button(name, value) {
-  this.name = name
-  this.value = value
-
-  this.draw = function() {
+  draw() {
     document.querySelector('#interface').innerHTML += `<a href="#" class="button" id="${this.name}">${this.value}</a>`;
   }
 
-  this.giveEventListener = function() {
+  giveEventListener() {
     document.querySelector(`#${this.name}`).addEventListener('click', handlePress)
   }
 }
 
-function evaluate() {
-  //equation must be of odd length and alternate num, op
-  let valid = equation.length % 2 !== 0 && equation.reduce((flag, c, i) => i % 2 === 0? flag && !isNaN(Number(c)): flag && /[+x/-]/.test(c), true) 
-  if (!valid) { 
-    reset = true
-    equation = ['Invalid expression']
-    return
+class Calculator {
+  constructor() {
+    this.equation = []
+    this.reset = true
+    this.buttons = [
+      new Button('seven', 7),
+      new Button('eight', 8),
+      new Button('nine', 9),
+      new Button('divide', '/'),
+      new Button('four', 4),
+      new Button('five', 5),
+      new Button('six', 6),
+      new Button('multiply', 'x'),
+      new Button('one', 1),
+      new Button('two', 2),
+      new Button('three', 3),
+      new Button('add', '+'),
+      new Button('zero', 0),
+      new Button('point', '.'),
+      new Button('equals', '='),
+      new Button('subtract', '-')
+    ]
+
+    this.buttons.forEach(b => {
+      b.draw()  
+    })
+    
+    this.buttons.forEach(b => {
+      b.giveEventListener()
+    })
   }
-  
-  //maintain proper order of operations
-  for (let i = 1; i < equation.length - 1; i++) {
-    if (equation[i] === 'x' || equation[i] === '/') {
-      equation[i] === 'x'
-        ? equation.splice(i-1, 3, Number(equation[i-1]) * Number(equation [i+1]))
-        : equation.splice(i-1, 3, Number(equation[i-1]) / Number(equation [i+1]))
-      --i;
+
+  evaluate() {
+    //equation must be of odd length and alternate num, op
+    let valid = this.equation.length % 2 !== 0 && this.equation.reduce((flag, c, i) => i % 2 === 0? flag && !isNaN(Number(c)): flag && /[+x/-]/.test(c), true) 
+    if (!valid) { 
+      this.reset()
+      this.equation = ['Invalid expression']
+      return
     }
-  }
-
-  for (let i = 1; i < equation.length - 1; i++) {
-    if (equation[i] === '+' || equation[i] === '-') {
-      equation[i] === '+'
-        ? equation.splice(i-1, 3, Number(equation[i-1]) + Number(equation [i+1]))
-        : equation.splice(i-1, 3, Number(equation[i-1]) - Number(equation [i+1]))
-      --i
-    }
-  }
-
-  //end of current calculation
-  reset = true
-}  
-
-function handlePress(event) {
-  if (reset) {
-    equation = []
-    reset = false
-  }
-
-  let value = event.target.innerText 
-  switch(value) {
-    case '+':
-    case '-':
-    case 'x':
-    case '/':
-      equation.push(value)
-      break;
-    case '=': evaluate()
-      break;
-    default: //digits
-      if (/\d|[.]/.test(equation[equation.length-1]) || /-/.test(equation[equation.length-1]) && equation.length % 2 === 1) {
-        equation[equation.length-1] += value 
-      } else {
-        equation.push(value)
+    
+    //maintain proper order of operations
+    for (let i = 1; i < this.equation.length - 1; i++) {
+      if (this.equation[i] === 'x' || this.equation[i] === '/') {
+        this.equation[i] === 'x'
+          ? this.equation.splice(i-1, 3, Number(this.equation[i-1]) * Number(this.equation [i+1]))
+          : this.equation.splice(i-1, 3, Number(this.equation[i-1]) / Number(this.equation [i+1]))
+        --i;
       }
-      break;
+    }
+  
+    for (let i = 1; i < this.equation.length - 1; i++) {
+      if (this.equation[i] === '+' || this.equation[i] === '-') {
+        this.equation[i] === '+'
+          ? this.equation.splice(i-1, 3, Number(this.equation[i-1]) + Number(this.equation [i+1]))
+          : this.equation.splice(i-1, 3, Number(this.equation[i-1]) - Number(this.equation [i+1]))
+        --i
+      }
+    }
+  
+    //end of current calculation
+    this.reset = true
   }
 
-  //display
-  document.querySelector('#display').innerText = equation.join(' ')
+  addToEquation(value) {
+    if(this.reset) {
+      this.reset = false
+      this.equation = []
+    }
+
+    switch(value) {
+      case '+':
+      case '-':
+      case 'x':
+      case '/':
+        this.equation.push(value)
+        break;
+      case '=': calc.evaluate()
+        break;
+      default: //digits
+        if (/\d|[.]/.test(this.equation[this.equation.length-1]) || /-/.test(this.equation[this.equation.length-1]) && this.equation.length % 2 === 1) {
+          this.equation[this.equation.length-1] += value 
+        } else {
+          this.equation.push(value)
+        }
+        break;
+    }
+  }
+
+  getEquationString() {
+    return this.equation.join(' ')
+  }
 }
 
-let buttons = [
-  new Button('seven', 7),
-  new Button('eight', 8),
-  new Button('nine', 9),
-  new Button('divide', '/'),
-  new Button('four', 4),
-  new Button('five', 5),
-  new Button('six', 6),
-  new Button('multiply', 'x'),
-  new Button('one', 1),
-  new Button('two', 2),
-  new Button('three', 3),
-  new Button('add', '+'),
-  new Button('zero', 0),
-  new Button('point', '.'),
-  new Button('equals', '='),
-  new Button('subtract', '-')
-]
+let calc = new Calculator()
 
-buttons.forEach(b => {
-  b.draw()  
-})
+function handlePress(event) {
+  let value = event.target.innerText 
+  calc.addToEquation(value)
 
-buttons.forEach(b => {
-  b.giveEventListener()
-})
+  //display
+  document.querySelector('#display').innerText = calc.getEquationString()
+}
+
+
