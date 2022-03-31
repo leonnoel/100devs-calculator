c68abc1ccb4b17849fd240b2e4c3d0ee98cf7893
Adam <96756923+ramblingadam@users.noreply.github.com>
2022-03-31 14:28:19 -0500

    oop attempt backup before replacement

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
+// pseudo code
+// 
+// 
+// user presses a number button, store it
+
+
+//Grab all the buttons into a node list
+buttonList = document.querySelectorAll('.btn')
+
+buttonList.forEach(btn => {
+    btn.addEventListener('click', )
+})
+
+console.log(typeof(buttonList[0].innerText))
+
+console.log(buttonList)
+
+// Add event listeners for buttons
+
+
+
 
 class Calculator {
+    
     constructor() {
 
     }
+    log() {
+        console.log('a thing')
+    }
 
-    
+
+
 }
