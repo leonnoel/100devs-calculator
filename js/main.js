47c6d4c0b4a3f0da8ad656faf74825e2bb869dc7
ryanpilar <ryan_pilar@outlook.com>
2022-03-30 20:08:10 -0600

    2nd commit, changes to methods to make it more 'best practise'

--- js/main.js
+++ js/main.js
@@ -1,91 +1,42 @@
-let Calc = {
-    screen: {
-        textField: '',
-        waiting: '',
-        tally: '',
-        operand: undefined,
-        calculated: 0,
-    }, 
-    addNum: (event) => {
-        let value = event.target.innerText
-        
-        // to prevent double decimals
-        if ( value === '.' ) {
-            Calc.screen.textField.indexOf(value) === -1 ? Calc.updateScreen(value) : false
-        }
-        // display new results 
-        else { Calc.updateScreen(value) }
+// ----------------------------------
+// Create a calculator
+// ----------------------------------
+
+function Calculator() {
+    // ... your code ...
+    this.first = undefined
+    this.second = undefined
+
+    this.read = function() {
+        this.first = Number(prompt('give me the first num you wanted added'))
+        this.second = Number(prompt('give the second num you want to add'))
     },
 
-    updateScreen: (value) => {
-        console.log(Calc.screen.textField)
-        
-        // anymore than 10 numbers, and the screen deforms
-        Calc.screen.textField.length < 10 ? Calc.screen.textField += `${value}` : false
-        
-        // display new results
-        document.querySelector('#screen').innerHTML = Calc.screen.textField
-    },
+    this.sum = function() { return this.first + this.second },
+    this.mul = function() { return this.first * this.second }
 
-    setOperand: (event) => {
 
-        let operand = event.target.innerText
-        console.log('Calc.screen.textField[-1]', Calc.screen.textField)
+  };
 
-        // don't allow an opperand to be the first entry
-        if ( Calc.screen.textField.length === 0 ) {
-            if (operand === 'x' || operand === '/') {
-                return console.log(false)
-            }
-        } else if( Calc.screen.textField.slice(-1) === '*' || Calc.screen.textField.slice(-1) === '/' ) {
-            return console.log(false)
-        }
+let calculator = new Calculator();
+//   calculator.read();
+//   alert( calculator.sum() );
+//   alert( calculator.mul() );
 
-        // mathJS cannot porcess 'x' so change to '*'
-        operand = operand === 'x' ? '*' : operand
 
-        // add the operand to the screen
-        Calc.screen.textField = Calc.screen.textField + operand
-        document.querySelector('#screen').innerHTML = Calc.screen.textField
+// ----------------------------------
+// Create a calculator
+// ----------------------------------
 
-        Calc.screen.operand = operand
-    },
-    doCalc: () => {
-        // plug string into MathJS
-        let calc = math.eval(Calc.screen.textField);
-        // anymore than 10 numbers, and the screen deforms
-        let setN = 10
-        if( calc.length < setN ) {}
-        else {
-            calc = calc.toFixed(3)
-        } 
-
-        // push results to screen and resent to a blank textField
-        document.querySelector('#screen').innerHTML = calc
-        Calc.screen.textField = ''
-    },
-    reset: () => {
-        Calc.screen.textField = ''
-        document.querySelector('#screen').innerHTML = ''
+function Accumulator(startingValue) {
+    this.value = startingValue
+    this.read = function() {
+        this.value += Number(prompt('add new number'))
     }
 }
-// -------------- NUMBERS --------------------
-document.querySelector('#zero').addEventListener('click', Calc.addNum)
-document.querySelector('#one').addEventListener('click', Calc.addNum)
-document.querySelector('#two').addEventListener('click', Calc.addNum)
-document.querySelector('#three').addEventListener('click', Calc.addNum)
-document.querySelector('#four').addEventListener('click', Calc.addNum)
-document.querySelector('#five').addEventListener('click', Calc.addNum)
-document.querySelector('#six').addEventListener('click', Calc.addNum)
-document.querySelector('#seven').addEventListener('click', Calc.addNum)
-document.querySelector('#eight').addEventListener('click', Calc.addNum)
-document.querySelector('#nine').addEventListener('click', Calc.addNum)
-document.querySelector('#decimal').addEventListener('click', Calc.addNum)
-// -------------- OPERANDS --------------------
-document.querySelector('#minus').addEventListener('click', Calc.setOperand)
-document.querySelector('#plus').addEventListener('click', Calc.setOperand)
-document.querySelector('#times').addEventListener('click', Calc.setOperand)
-document.querySelector('#divide').addEventListener('click', Calc.setOperand)
-document.querySelector('#equals').addEventListener('click', Calc.doCalc)
-// --------------   RESET  --------------------
-document.querySelector('#reset').addEventListener('click', Calc.reset)
+let accumulator = new Accumulator(1); // initial value 1
+
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+alert(accumulator.value); // shows the sum of these values
