8dd610df4a8b1adfb4dfbce24a55c18ab9eb8653
kgni <admin@mdia.dk>
2022-04-01 00:33:23 +0200

    select buttons in JS

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 // Have an input window, where all values and math operations are going to show up. When the equal sign is pressed, all the math operations that has been made is going to be evaluated and put into a sum variable, that will display
 
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+
+const allClearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalsButton = document.querySelector('[data-equals]');
+
 class Calculator {
 	constructor(previousOperand, currentOperand) {
 		this.previousOperand = previousOperand;
