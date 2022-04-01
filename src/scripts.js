9396dde8601cfbb1760a43619ca4a9f9c2debe4f
John Malapit <johncmalapit@gmail.com>
2022-04-01 15:00:01 -0400

    create operation functions

---
+++ src/scripts.js
@@ -0,0 +1,28 @@
+let calculator = {
+  add: function(a,b){
+    return a + b
+  },
+  subtract: function(a,b){
+    return a - b
+  },
+  multiply: function(a,b){
+    return a * b
+  },
+  divide: function(a,b){
+    return a / b
+  },
+  appendNumber(number){
+
+  },
+  chooseOperation(operation){
+
+  },
+  calculate(){
+
+  },
+  updateDisplay(){
+    
+  }
+}
+
+console.log(calculator.add(1,2))
