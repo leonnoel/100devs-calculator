e2630de097232c1b97da4214482ddade54931eaa
Celia Kelly <celiackelly@gmail.com>
2022-04-01 11:55:46 -0400

    Switch to simple output (no previous/current operand).

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+/*  Pseudo Code
+- Event listeners on number buttons 
+    - on click, add number to output innerHTML
+    
+- Event listeners on operator buttons
+    - on click, 
 
+- AC
+
+- +/- 
+
+- % 
+
+
+
+
+
+*/
