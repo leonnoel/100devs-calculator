544c5bbc54745e2c1fa61c5ce71829ac7da4ad38
Ines Gvoic <igvoic.work@gmail.com>
2022-04-01 15:22:45 -0700

    oop final

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+// EVENT HANDLERS
+
 document.querySelectorAll('.buttons').forEach(item => {     // numerical values buttons
     item.addEventListener("click", clickedNumberEventHandler)
 });
@@ -11,6 +13,8 @@ document.querySelector('.dot').addEventListener("click", doDecimalNumbersEventHa
 document.getElementById('finalCalculation').addEventListener("click", calculatingEventHandler)   //  equal button/calculation
 
 document.querySelector('.clear').addEventListener("click", allCLearEventHandler) // clear everything
+
+// CALCULATOR OBJECT
 
 let basicCalculator = {
 
@@ -27,7 +31,7 @@ let basicCalculator = {
         }  
         this.leftNumber = this.leftNumber.substring(0,5)
         this.rightNumber = this.rightNumber.substring(0,5)  
-        this.str = this.leftNumber + this.operator + this.rightNumber
+        this.stringDisplay = this.leftNumber + this.operator + this.rightNumber
         document.querySelector(".result").innerText = this.stringDisplay
     },
 
@@ -79,12 +83,10 @@ let basicCalculator = {
     }
 }
 
+// FUNCTIONS CALLING CALCULATOR OBJECT METHODS
+
 function allCLearEventHandler(){
     basicCalculator.allClear(this.value)
-}
-
-function clearResultClone(){
-    basicCalculator.clearResult(this.value)
 }
 
 function calculatingEventHandler(){
@@ -102,81 +104,3 @@ function doDecimalNumbersEventHandler(){
 function clickedNumberEventHandler(){
     basicCalculator.clickedNumber(this.value)
 }
-
-/*document.querySelectorAll('.buttons').forEach(item => {     // numerical values buttons
-    item.addEventListener("click", clickedNumber)
-});
-
-document.querySelectorAll('.operators').forEach(item => {     // operator buttons
-    item.addEventListener("click", clickedOperator)
-});
-
-document.querySelector('.dot').addEventListener("click", doDecimalNumbers)   //dot, decimal numbers
-
-document.getElementById('finalCalculation').addEventListener("click", calculating)   //  equal button/calculation
-
-document.querySelector('.clear').addEventListener("click", allClear) // clear everything
-
-let str = ""
-let leftNumber = ""
-let rightNumber = ""
-let operator = ""
-
-function clickedNumber() {
-    if(operator === "") {
-        leftNumber += this.value
-    } else {
-        rightNumber += this.value
-    }  
-    leftNumber = leftNumber.substring(0,5)
-    rightNumber = rightNumber.substring(0,5)  
-    str = leftNumber + operator + rightNumber
-    document.querySelector(".result").innerText = str
-}
-
-function doDecimalNumbers () {
-    if (leftNumber !== "" && operator === "" && !leftNumber.includes(".")) {
-        leftNumber += "."
-    } else if (operator !== "" && rightNumber !=="" && !rightNumber.includes(".")) {
-        rightNumber += "."
-    }
-    str = leftNumber + operator + rightNumber
-    document.querySelector(".result").innerText = str
-}
-
-function clickedOperator () {
-    if (leftNumber !== "" && rightNumber === "") {
-        operator = this.value
-    }
-    str = leftNumber + operator + rightNumber
-    document.querySelector(".result").innerText = str
-}
-
-function calculating () {
-    let numberResult = ""
-    if(leftNumber === "" || rightNumber=== "" || operator=== "") {
-        return 
-    } else if (operator === '+') {
-        numberResult = Number(leftNumber) + Number(rightNumber)
-    } else if(operator === '-') {
-        numberResult = Number(leftNumber) - Number(rightNumber)
-    } else if(operator === 'x') {
-        numberResult = Number(leftNumber) * Number(rightNumber)
-    } else if(operator === '/') {
-        numberResult = Number(leftNumber) / Number(rightNumber)
-    }
-    document.querySelector(".result").innerText = Number(numberResult)
-    clearResult()
-}
-
-function clearResult() {
-    str = ""
-    leftNumber = ""
-    rightNumber = ""
-    operator = ""
-}
-
-function allClear () {
-    clearResult ()
-    document.querySelector(".result").innerText = ""
-} */
