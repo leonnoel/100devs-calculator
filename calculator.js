c3b885668894e984e9270611884d0ef288eb3f8f
Ines Gvoic <igvoic.work@gmail.com>
2022-04-01 05:49:06 -0700

    updated calculations

--- calculator.js
+++ calculator.js
@@ -1,9 +1,39 @@
-function logIt(){
-    let firstNumber = (this).value
-    document.querySelector(".result").innerText=firstNumber
+document.querySelectorAll('.buttons').forEach(item => {     // numerical values buttons
+    item.addEventListener("click", clickedNumber)
+});
+
+document.querySelectorAll('.operators').forEach(item => {     // operator buttons
+    item.addEventListener("click", clickedOperator)
+});
+
+//document.querySelector('.dot').addEventListener("click", getButtonValues)   //dot, decimal numbers
+
+document.getElementById('finalCalculation').addEventListener("click", calculating)   //  equal button/calculation
+
+
+let str = ""
+let leftNumber = ""
+let rightNumber = ""
+let operator = ""
+function clickedNumber() {
+    if(operator === "") {
+        leftNumber += this.value
+    } else {
+        rightNumber += this.value
+    }   
+    str = leftNumber + operator + rightNumber
+    document.querySelector(".result").innerText = str
 }
 
-document.querySelectorAll('.buttons').forEach(item => {
-    item.addEventListener("click", logIt)
-});
+function clickedOperator () {
+    if (leftNumber !== "" && rightNumber === "") {
+        operator = this.value
+    }
+    str = leftNumber + operator + rightNumber
+    document.querySelector(".result").innerText = str
+}
+
+function calculating () {
+    
+}
 
