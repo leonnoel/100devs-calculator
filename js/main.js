35a911b50108da7116d153064e690599d11b22a3
Celia Kelly <celiackelly@gmail.com>
2022-04-01 19:46:43 -0400

    Add Calculator constructor with basic operations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /*  Pseudo Code
 - Event listeners on number buttons 
     - on click, add number to output innerHTML
+    - 
     
 - Event listeners on operator buttons
     - on click, 
@@ -11,8 +12,25 @@
 
 - % 
 
-
-
-
-
 */
+
+function Calculator() {
+    
+    this.add = (n1, n2) => n1 + n2
+    this.subtract = (n1, n2) => n1 - n2
+    this.multiply = (n1, n2) => n1 + n2
+    this.divide = (n1, n2) => n1 / n2
+    this.changeSign = n1 => Math.sign(n1) === 1 ? -n1 : Math.abs(n1)
+    this.percent = n1 => n1 / 100
+    this.clear = () => {
+        n1 = 0 
+        n2 = 0
+    }
+
+}
+
+const calc = new Calculator
+
+let result = calc.changeSign(-4)
+
+console.log(result)
