37fbe4e7c89eb5573293bf9fcffad9ca5533a729
John Malapit <johncmalapit@gmail.com>
2022-04-02 18:58:53 -0400

    complete calculator logic

--- src/scripts.js
+++ src/scripts.js
@@ -1,28 +1,77 @@
-let calculator = {
-  add: function(a,b){
-    return a + b
-  },
-  subtract: function(a,b){
-    return a - b
-  },
-  multiply: function(a,b){
-    return a * b
-  },
-  divide: function(a,b){
-    return a / b
-  },
-  appendNumber(number){
+function Calculator() {
+  this.temp = 0;
+  this.num1 = 0;
+  this.num2 = 0;
+  this.operator = null;
 
-  },
-  chooseOperation(operation){
+  this.appendNumber = function(number){
+    this.temp == 0 ? this.temp = number : this.temp = this.temp.toString()+number.toString()
+  }
 
-  },
-  calculate(){
+  this.appendDecimal = function(){
+    if(!this.temp.includes('.')){
+      this.temp = this.temp.toString()+'.'
+    }
+  }
 
-  },
-  updateDisplay(){
-    
+  this.chooseOperation = function(operation){
+    this.operator = operation
+  }
+
+  this.calculate = function(){
+    switch(this.operator){
+      case '+':
+        return Number(this.num1) + Number(this.num2)
+      case '-':
+        return this.num1 - this.num2
+      case '*':
+        return this.num1 * this.num2
+      case '/':
+        return this.num1 / this.num2
+    }
+  }
+
+  this.updateDisplay = function(){
+    document.querySelector('#display').innerText = this.temp
   }
 }
 
-console.log(calculator.add(1,2))
+const calculator = new Calculator();
+
+const numberButtons = document.querySelectorAll('.numberButton')
+numberButtons.forEach(b => b.addEventListener('click', e => {
+  calculator.appendNumber(e.target.innerText);
+  calculator.updateDisplay()
+}))
+
+const operatorButtons = document.querySelectorAll('.operatorButton')
+operatorButtons.forEach(b => b.addEventListener('click', e => {
+  if (calculator.num1){
+    calculator.num2 = calculator.temp
+    calculator.temp = calculator.calculate()
+    calculator.updateDisplay()
+    calculator.chooseOperation(e.target.innerText)
+    calculator.num1 = calculator.temp
+    calculator.temp = 0
+  } else {
+    calculator.chooseOperation(e.target.innerText)
+    calculator.num1 = calculator.temp
+    calculator.temp = 0
+  }
+}))
+
+const decimalButton = document.querySelector('#decimal').addEventListener('click', e => {
+  calculator.appendDecimal();
+  calculator.updateDisplay();
+})
+
+const equalButton = document.querySelector('#equal').addEventListener('click', e => {
+  if (calculator.operator){
+    calculator.num2 = calculator.temp
+    calculator.temp = calculator.calculate()
+    calculator.updateDisplay()
+    calculator.num1 = 0
+    calculator.num2 = 0
+    calculator.operator = null;
+  }
+})
