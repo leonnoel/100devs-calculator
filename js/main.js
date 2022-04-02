56208b6432d0382a9a37c19d636d0c9dccf71efa
Matt <Matthew.hung01@gmail.com>
2022-04-02 01:25:10 -0400

    holy shit it works

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //class for NumberButton, Mult, Divide, Add, Subtract, Equals
-//
 //function for calculating: math.evaluate(string)
 //when a number or operator button is pressed, add the innertext to the string
 //if the button is the decimal or an operator, check if equation.endsWith('') any operator or decimal maybe use string.match(regex)
@@ -8,7 +7,8 @@
 //call constructors to create const for each button
 //function to clear equation
 //after an equation is evaluated, the next button press should call the clear function before generating the new equation
-
+//i came across the math.js evaluate function while planning this out and didn't realize it was from an external library.
+//so since i came this far already (i coded the actual calculator part last) there's no going back now. i'm still going to use math.js
 
 let calcScreen = document.querySelector("#calcScreen");
 let equation = "";
@@ -16,6 +16,7 @@ function clear() {
     equation = "";
     calcScreen.innerText = equation;
 }
+let reset = false;
 
 
 //I wanted to hide the above stuff in a Calculator class, but when I do that and extend numberbutton from the calculator class,
@@ -30,9 +31,13 @@ class CalculatorButton {
 }
 class NumberButton extends CalculatorButton {
     constructor(element) {
-        super(element)
+        super(element) //oh, i understand what it meant by super constructors now
         this.element.addEventListener('click', run)
         function run() {
+            if (reset == true) { 
+                clear();
+                reset = false;
+            }
             if (equation.length < 9 || (CalculatorButton.operators.test(equation) && equation.length < 11)) {
                 equation += this.innerText;
                 calcScreen.innerText = equation;
@@ -45,6 +50,10 @@ class OperatorButton extends CalculatorButton {
         super(element)
         this.element.addEventListener('click', run)
         function run() {
+            if (reset == true) {
+                clear();
+                reset = false;
+            }
             if (equation.length == 0) {
                 return;
             } else if (equation.length < 9 && !CalculatorButton.regex.test(equation)) {
@@ -61,8 +70,12 @@ class EqualsButton extends CalculatorButton {
         super(element)
         this.element.addEventListener('click', evaluate)
         function evaluate() {
-            equation = math.evaluate(equation);
-            calcScreen.innerText = equation;
+            if (equation.length) {
+                equation = math.evaluate(equation);
+                //is it cheating to use a library for this? 
+                calcScreen.innerText = equation.toString().substring(0, 13);
+                reset = true;
+            } else return;
         }
     }
 }
