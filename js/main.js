d56b9f8c92fd38a28b808b4e37e9367ae94ef88c
Dylan Longstreet <longstreet.dylan@gmail.com>
2022-04-02 16:15:37 -0400

    Implemented "equals" button functionality with operator buttons ( / * + - )
    added "right-hand-side" value storage and current operator storage.
    
    If the user enters a number followed by an operator, the program awaits the entry of a right-hand-side number.
    
    When the equals (=) button is selected and there is a current operator and a right-hand-value, the calculate() function is called and the total is stored in memory and displayed.
    
    Selecting equals again will use the same operator and right-hand-value that is stored in memory (equals-chaining)
    
    What needs to be added:
    Operator chaining, such that the user can enter right-hand-values and select operators in sequence without having to select the equals button to calculate.
    
    The decimal place button is not implemented, so you cannot currently use the number pad buttons to add a decimal point to the input value.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-// store the current value stored in calculator working memory
-let currentTotal = 0
+// store the current value stored in calculator working memory, and the current operator if selected
+let currentTotal = 0 // the current total stored in memory
+let currentOperator = "" // the current operator (/*+-) stored in memory
+let currentRightHandSideValue = NaN // the current value on the right side of the current operator, stored in memory
+let currentTotalDisplayed = false
 
 // store the input field, numbers can be typed in or added using the number buttons
 const inputField = document.getElementById("inputField")
@@ -27,7 +30,7 @@ function buttonPressed(buttonText) {
     }
 
     // with any other button, append the inner text to the current equation
-    appendToCurrentInput(buttonText)
+    processInput(buttonText)
 }
 
 // returns true if the value is an operator character (/*+-)
@@ -38,12 +41,46 @@ function isOperator(value) {
 }
 
 // each time a button is pressed, append the value (and the input field value if applicable) to the current equation string
-function appendToCurrentInput(valueToAppend) {
+function processInput(newInput) {
+    const operatorValue = isOperator(newInput)
     // if current input is empty, disregard any attempt to engage an operator (/*+-)
-    if(inputField.value === "" && isOperator(valueToAppend)) { return }
-    // otherwise, append the value to the input field
+    if(inputField.value === "" && operatorValue) { return }
+
+    // if an operator was selected after another operator already active, change the current operator and return
+    if(operatorValue && currentOperator != "") {
+        console.log(`current operator ${currentOperator} replaced with ${newInput}`)
+        currentOperator = newInput
+    }
+
+    // if the input is not empty and an operator was selected...
+    if(operatorValue) {
+        const tempInputNumber = parseFloat(inputField.value)
+
+        // if the parsed number is NaN, return out
+        if(isNaN(tempInputNumber)) { 
+            console.log(`inputField value is ${tempInputNumber}!`)
+            return
+        }
+
+        // if the right-hand-value total already has a value stored, selecting an operator will cause a calculate() with the current operator before updating to the new operator (operator chaining)
+        /*if(currentOperator != "" && !isNaN(currentRightHandSideValue)) {
+            console.log("Operator chain - calculate results with stored RHS value")
+            calculate()
+            currentOperator = newInput
+            return
+        }*/
+
+        // otherwise, update the current total to the input field value, store the current operator, and clear the input field
+        currentOperator = newInput
+        currentTotal = tempInputNumber
+        inputField.value = ""
+        return
+    }
+
+    // otherwise the value is a number or decimal point, so append the value to the input field
+
     //console.log(`Input Value before: (${inputField.value}) valueToAppend: (${valueToAppend})`)
-    let appendedValue = inputField.value.concat(valueToAppend)
+    let appendedValue = inputField.value.concat(newInput)
     //console.log(`Appended Value: ${appendedValue}`)
 
     parsedNumber = parseFloat(appendedValue)
@@ -55,11 +92,55 @@ function appendToCurrentInput(valueToAppend) {
     // otherwise check maxlength, then assign the parsed number back to the input field value
     if (inputField.value.length < inputField.maxLength) {
         inputField.value = parsedNumber
-        //console.log(`Input Value after: ${inputField.value}`)
+        currentTotalDisplayed = false
     }
 }
 
 // when the user selects "Equals", append the final input field value and parse the current equation
 function calculate() {
     // parse the current equation into numeric arithmetic, calculate the result and display it to the input field
+    let tempInputNumber = parseFloat(inputField.value)
+    let calculatedValue = 0
+
+     // if the parsed number is NaN, return out
+    if(isNaN(tempInputNumber)) { 
+         console.log(`inputField value is ${tempInputNumber}!`)
+        return
+    }
+
+    // if there is no current operator, return out
+    if(currentOperator === "") { return }
+
+    // if there is already a right-hand-side value (equals button chaining), use it as the RHS value in the arithmetic
+    if(!isNaN(currentRightHandSideValue) && currentTotalDisplayed) {
+        tempInputNumber = currentRightHandSideValue
+    }
+
+    console.log(`Current Equation: ${currentTotal} ${currentOperator} ${tempInputNumber}`)
+    // Perform the arithmetic on the two values
+    switch (currentOperator) {
+        case "/":
+            calculatedValue = currentTotal / tempInputNumber
+            break;
+        case "*":
+            calculatedValue = currentTotal * tempInputNumber
+            break;
+        case "+":
+            calculatedValue = currentTotal + tempInputNumber
+             break;
+        case "-":
+            calculatedValue = currentTotal - tempInputNumber
+            break;
+        default:
+            console.log(`invalid operator: ${currentOperator}!`)
+            break;
+    }
+
+    console.log(`Equation Result: ${calculatedValue}`)
+
+    // update the stored current total value, display it in the input field, reset the current operator
+    currentRightHandSideValue = tempInputNumber
+    currentTotal = calculatedValue
+    inputField.value = currentTotal
+    currentTotalDisplayed = true
 }
