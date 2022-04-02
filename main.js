82f4e9e0cb7068c917cbf1cd0148451b82986f25
kgni <admin@mdia.dk>
2022-04-02 10:51:34 +0200

    add computation

--- main.js
+++ main.js
@@ -1,5 +1,98 @@
 // Have an input window, where all values and math operations are going to show up. When the equal sign is pressed, all the math operations that has been made is going to be evaluated and put into a sum variable, that will display
 
+class Calculator {
+	constructor(previousOperandTextElement, currentOperandTextElement) {
+		this.previousOperandTextElement = previousOperandTextElement;
+		this.currentOperandTextElement = currentOperandTextElement;
+		this.clear();
+	}
+
+	clear() {
+		this.currentOperand = '';
+		this.previousOperand = '';
+		this.operation = undefined;
+	}
+
+	delete() {
+		this.currentOperand = this.currentOperand.toString().slice(0, -1);
+	}
+
+	appendNumber(number) {
+		if (number === '.' && this.currentOperand.includes('.')) return;
+		this.currentOperand = this.currentOperand.toString() + number.toString();
+	}
+
+	chooseOperation(operation) {
+		if (this.currentOperand === '') return;
+		if (this.previousOperand !== '') {
+			this.compute();
+		}
+
+		this.operation = operation;
+		this.previousOperand = this.currentOperand;
+		this.currentOperand = '';
+	}
+
+	compute() {
+		let computation;
+		const prev = parseFloat(this.previousOperand);
+		const current = parseFloat(this.currentOperand);
+		if (isNaN(prev) || isNaN(current)) return;
+		switch (this.operation) {
+			case '+':
+				computation = prev + current;
+				break;
+			case '-':
+				computation = prev - current;
+				break;
+			case '*':
+				computation = prev * current;
+				break;
+			case '÷':
+				computation = prev / current;
+				break;
+			default:
+				return;
+		}
+
+		this.currentOperand = computation;
+		this.operation = undefined;
+		this.previousOperand = '';
+	}
+
+	getDisplayNumber(number) {
+		const stringNumber = number.toString();
+		const integerDigits = parseFloat(stringNumber.split('.')[0]);
+		const decimalDigits = stringNumber.split('.')[1];
+		let integerDisplay;
+		if (isNaN(integerDigits)) {
+			integerDisplay = '';
+		} else {
+			integerDisplay = integerDigits.toLocaleString('en', {
+				maximumFractionDigits: 0,
+			});
+		}
+		if (decimalDigits != null) {
+			return `${integerDisplay}.${decimalDigits}`;
+		} else {
+			return integerDisplay;
+		}
+	}
+
+	updateDisplay() {
+		this.currentOperandTextElement.innerText = this.getDisplayNumber(
+			this.currentOperand
+		);
+		if (this.operation != null) {
+			this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+				this.previousOperand
+			)} ${this.operation}`;
+		} else {
+			this.previousOperandTextElement.innerText = '';
+		}
+	}
+}
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 
@@ -13,26 +106,33 @@ const currentOperandTextElement = document.querySelector(
 	'[data-current-operand]'
 );
 
-class Calculator {
-	constructor(previousOperand, currentOperand) {
-		this.previousOperand = previousOperand;
-		this.currentOperand = currentOperand;
-		this.operation = undefined;
-		this.clear();
-	}
+const calculator = new Calculator(
+	previousOperandTextElement,
+	currentOperandTextElement
+);
 
-	clear() {
-		this.currentOperand = '';
-		this.previousOperand = '';
-	}
+numberButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		calculator.appendNumber(button.innerText);
+		calculator.updateDisplay();
+	});
+});
+operationButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		calculator.chooseOperation(button.innerText);
+		calculator.updateDisplay();
+	});
+});
 
-	delete() {}
-
-	appendNumber(number) {}
-
-	chooseOperation(operation) {}
-
-	compute() {}
-
-	updateDisplay() {}
-}
+equalsButton.addEventListener('click', (button) => {
+	calculator.compute();
+	calculator.updateDisplay();
+});
+allClearButton.addEventListener('click', (button) => {
+	calculator.clear();
+	calculator.updateDisplay();
+});
+deleteButton.addEventListener('click', (button) => {
+	calculator.delete();
+	calculator.updateDisplay();
+});
