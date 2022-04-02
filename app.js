a718c072914474dfdf2a461af966440c35c5ef92
Michael-Baas <michaelbaas@me.com>
2022-04-02 19:20:46 -0400

    Add compute and chooseOperand method

--- app.js
+++ app.js
@@ -14,19 +14,53 @@ class Calculator{
     appendNumber(number){
         if(number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString();
-       
     }
 
-    calculate(){
-
+    chooseOperand(operation){
+        if(this.currentOperand === '') return 
+        if(this.previousOperand !== ''){
+            this.compute()
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+j
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if(isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+' :
+                computation = prev + current
+                break
+            case '-' :
+                computation = prev - current
+                break
+            case '*' :
+                computation = prev * current
+                break
+            case '/' :
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = ''
     }
     
     updateDisplay(){
-        console.log(this.currentOperand, this.currentOperandElement)
         this.currentOperandElement.innerText = this.currentOperand;
+        if(this.operation != null){
+            this.previousOperandElement.innerText = `${this.previousOperand} ${this.operation}`
+        }else {
+            this.previousOperandElement.innerText = ''
+        }
     }
 }
-
 // Create variables for DOM elements
 const numberButtons = document.querySelectorAll('[data-number]');
 const operatorButtons = document.querySelectorAll('[data-operator]');
@@ -36,6 +70,8 @@ const currentOperandElement = document.querySelector('[data-current-operand]');
 
 const calculator = new Calculator(previousOperandElement, currentOperandElement)
 
+
+//Listen for numbers
 numberButtons.forEach(button => {
     button.addEventListener('click', () =>{
         calculator.appendNumber(button.innerText);
@@ -43,3 +79,19 @@ numberButtons.forEach(button => {
         
     })
 })
+
+// Listen for operators 
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        calculator.chooseOperand(button.innerText);
+        calculator.updateDisplay();
+        
+    })
+})
+
+// Evaluate calculation 
+equalsButton.addEventListener('click', () => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
