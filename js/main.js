89ebbd8c6d15f6e6bd206594cbb5762d301aed6e
Yuan Xie <luckybellaxie@gmail.com>
2022-04-03 08:31:37 -0400

    save

--- js/main.js
+++ js/main.js
@@ -1,23 +1,48 @@
 let buttons = document.querySelectorAll('.button')
-Array.from(buttons).forEach(e => e.addEventListener('click',calculate)) 
+Array.from(buttons).forEach(e => e.addEventListener('click',calculate))
+
+let clearInit = true;
+let clearLater = false;
 
 function calculate(e) {
-    let value = e.target.getAttribute('value')
-    let displayed = document.querySelector('.display-box').getAttribute('value')
-    if (value === '=') {
-        const inputCal = displayed.split(' ');
-        console.log(...inputCal)
-        let calculator = new Calculator(...inputCal);
-        displayedEqual = calculator.calculate();
-        document.querySelector('.display-box').setAttribute('value',displayedEqual)
-    } 
-    else if (value === 'C') {
-        document.querySelector('.display-box').setAttribute('value','')
-    } else {
-        const operators = ['+','-','*','/'];
-        let valueNew = operators.includes(value)?` ${value} `:value;
-        document.querySelector('.display-box').setAttribute('value',displayed+valueNew)
+    let value = e.target.getAttribute('value');
+    let displayed = document.querySelector('.display-box').getAttribute('value');
+    const operators = ['+','-','*','/'];
+    const special =  ['+','-','*','/','='];
+    console.log(clearLater);
+    if (clearLater) {
+        document.querySelector('.display-box').setAttribute('value','');
+        displayed = '';
     }
+    if (clearInit && special.includes(value) && clearLater) {
+        document.querySelector('.display-box').setAttribute('value','');
+    } else {
+        // console.log('here')
+        if (value === '=') {
+            const inputCal = displayed.split(' ');
+            // console.log(...inputCal)
+            let calculator = new Calculator(...inputCal);
+            displayedEqual = calculator.calculate();
+            document.querySelector('.display-box').setAttribute('value',displayedEqual);
+            clearLater = true;
+            clearInit = false;
+        } 
+        else if (value === 'C') {
+            document.querySelector('.display-box').setAttribute('value','')
+            clearInit = true;
+        } else if (operators.includes(value)) {
+            value = ' ' + value + ' ';
+            document.querySelector('.display-box').setAttribute('value',displayed+value)
+            clearLater = false;
+            clearInit = false
+        } else {
+            // console.log('add')
+            document.querySelector('.display-box').setAttribute('value',displayed+value);
+            clearLater = false;
+            clearInit = false;
+        }
+    }
+    
 }
 
 function Calculator(a,operator,b) {
