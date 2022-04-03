b4f3d78c830817c2b30d8e98d145233faf5c141d
heyjochen <jochen@stierberger.com>
2022-04-03 15:57:53 -0500

    create constants

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const numberButton = document.querySelectorAll('[data-number')
+const operationsButton = document.querySelectorAll('[data-operator')
+const equalButton = document.querySelectorAll('[data-equal')
+const clearButton = document.querySelectorAll('[data-clear')
+const previousValueH2 = document.querySelectorAll('[data-previousvalue')
+const displayValueH2 = document.querySelectorAll('[data-displayvalue')
