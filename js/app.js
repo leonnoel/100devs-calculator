89a7209d9748460fcd1ea97ff62fc9dfd685b8e7
admin 365 <ujbolivar@gmail.com>
2022-04-03 20:16:58 +0200

    currently stumped on the js, I can only get the first button to give me its info

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-function operation() {
-    let num = Number(document.getElementsByClassName('.number').innerText);
-    let operator = document.getElementsByClassName('.operator').innerText;
-    let decimal = document.getElementsByClassName('.decimal').innerText;   
+let button = document.querySelector('.button');
+
+function onScreen() {
+    document.querySelector('#screen').innerText = button.innerText
 }
+    button.addEventListener('click' , onScreen)
