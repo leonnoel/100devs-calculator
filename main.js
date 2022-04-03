52cc638746c0384ae54ee0716b29fc7fcd51908a
Kate Ives <katie.ives30@gmail.com>
2022-04-03 18:28:14 -0400

    add html, css and js files
    added some basic html and css layout for the calculator.
    
    slightly modified the calculator object code from one of the hw examples. nothing works yet

---
+++ main.js
@@ -0,0 +1,33 @@
+
+let calc = new Calculator;
+
+let string = "12 * 12 + 1";
+
+function Calculator() {
+
+  this.methods = {
+    "-": (a, b) => a - b,
+    "+": (a, b) => a + b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+    "**": (a, b) => a ** b
+  };
+
+  this.calculate = function(str) {
+
+    let split = str.split(' '),
+      a = +split[0],
+      op = split[1],
+      b = +split[2];
+
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
+    }
+
+    return this.methods[op](a, b);
+  };
+
+}
+
+console.log(calc.calculate(string)); // 10
+
