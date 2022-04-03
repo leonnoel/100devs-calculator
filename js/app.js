403dfea976b3d9a58a3988f88392de722ac2badd
admin 365 <ujbolivar@gmail.com>
2022-04-03 18:49:18 +0200

    created all the folders and files, finished with index html and main css

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function operation() {
+    let num = Number(document.getElementsByClassName('.number').innerText);
+    let operator = document.getElementsByClassName('.operator').innerText;
+    let decimal = document.getElementsByClassName('.decimal').innerText;   
+}
