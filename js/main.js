d869d875fcc173b3508ee732ccf919fd34d1159b
Dave DeVries <davriziod@gmail.com>
2022-04-04 09:33:16 -0400

    started psuedocode

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+//get button clicks and assign value
+let number = document.getElementsByClassName('nums')
+document.getElementsByClassName('result').innerText = number
+//return number button clicks to the dom
 
+//determine which operand was clicked
+
+//run the appropriate math operation
