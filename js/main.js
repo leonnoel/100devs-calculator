e5a9416b6b8c9617ed0af88c70fefa660761d1cc
Ben Thrasher <ben.r.thrasher@gmail.com>
2022-04-04 19:46:54 -0400

    more calc JS

--- js/main.js
+++ js/main.js
@@ -6,9 +6,51 @@ class Calculator {
     this.operator = null;
   }
 
+  parseInput(value) {
+    if (this.displayValue === "0") {
+      this.displayValue = "";
+    }
+
+    switch (value) {
+      case "=":
+        //calculate answer
+        break;
+      case "AC":
+        //clear screen and stored values
+        break;
+      case ".":
+        if (this.displayValue == 0) {
+          //pass '0.' into add text method
+        } else {
+          //add value to text string
+        }
+    }
+  }
+
   updateDisplay() {
     const display = document.querySelector(".calculator-screen");
 
     display.value = this.displayValue;
   }
+
+  setDisplayValue(value) {
+    this.displayValue = value;
+  }
 }
+
+const testCalc = new Calculator();
+
+console.log("present");
+
+const keys = document.querySelector(".calculator-keys");
+
+keys.addEventListener("click", (event) => {
+  const { target } = event;
+  const { value } = target;
+
+  if (!target.matches("button")) {
+    return;
+  } else {
+    testCalc.parseInput(value);
+  }
+});
