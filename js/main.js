75d906ece8cf7b0a239a604a9becdff09b767422
Celia Kelly <celiackelly@gmail.com>
2022-04-03 21:30:07 -0400

    Improve code comments

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ function Calculator() {
     this.percent = n1 => n1 / 100
 }
 
-//Calculator Interface Constructor function, to handle DOM manipulation
+//Calculator Interface Constructor function, to handle DOM manipulation and storing operand values
 function CalculatorInterface() {
 
     let previousOperand =  ''
@@ -43,7 +43,7 @@ function CalculatorInterface() {
     this.isOperatorSet = false
     const output = document.querySelector('.output')
 
-    //Add clicked number to current operand and show in output div
+    //Concatenate clicked number to current operand and show in output div
     this.concatenateOperand = (n) => {
         currentOperand += n
         output.innerHTML = currentOperand
@@ -67,28 +67,6 @@ function CalculatorInterface() {
         console.log(operator)
     }
 
-    //Changes sign in DOM and interface variables
-    this.changeSign = () => {
-        
-        if (!currentOperand) {
-            previousOperand = calc.changeSign(parseFloat(previousOperand))
-            output.innerHTML = previousOperand
-        } else {
-            currentOperand = calc.changeSign(parseFloat(currentOperand))
-            output.innerHTML = currentOperand
-        } 
-    }
-
-    this.percent = () => {
-        if (!currentOperand) {
-            previousOperand = calc.percent(parseFloat(previousOperand))
-            output.innerHTML = previousOperand
-        } else {
-            currentOperand = calc.percent(parseFloat(currentOperand))
-            output.innerHTML = currentOperand
-        } 
-    }
-
     //Perform calculations by calling calculator methods
     this.performCalculation = () => {
         console.log(previousOperand, operator, currentOperand)
@@ -101,6 +79,29 @@ function CalculatorInterface() {
         } 
         output.innerHTML = result
         currentOperand = result
+    }
+
+    //Change sign in output and operand
+    this.changeSign = () => {
+        
+        if (!currentOperand) {
+            previousOperand = calc.changeSign(parseFloat(previousOperand))
+            output.innerHTML = previousOperand
+        } else {
+            currentOperand = calc.changeSign(parseFloat(currentOperand))
+            output.innerHTML = currentOperand
+        } 
+    }
+
+    //Convert to percent and show in output
+    this.percent = () => {
+        if (!currentOperand) {
+            previousOperand = calc.percent(parseFloat(previousOperand))
+            output.innerHTML = previousOperand
+        } else {
+            currentOperand = calc.percent(parseFloat(currentOperand))
+            output.innerHTML = currentOperand
+        } 
     }
 
     //Reset all interface variables and output div text
