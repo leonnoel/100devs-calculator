8f354d7e31d7739a470c5c83466b28dfc6e2db4a
heyjochen <jochen@stierberger.com>
2022-04-04 11:40:14 -0500

    made numbers work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,53 @@
+
+class Calculator {
+    constructor(previousValueTextElement, displayValueTextElement) {
+        this.previousValueTextElement = previousValueTextElement
+        this.displayValueTextElement = displayValueTextElement
+        this.clear() 
+    }
+
+    clear() {
+        this.previousValue = ''
+        this.displayValue = ''
+        this.operation = undefined
+    }
+
+    appendNum (number) {
+        this.displayValue = number
+    }
+
+    chooseOperation (operator) {
+
+    }
+
+    equals(){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+        this.displayValueTextElement.innerText = this.displayValue
+    }
+
+}
+
+
 const numberButton = document.querySelectorAll('[data-number')
 const operationsButton = document.querySelectorAll('[data-operator')
-const equalButton = document.querySelectorAll('[data-equal')
-const clearButton = document.querySelectorAll('[data-clear')
-const previousValueH2 = document.querySelectorAll('[data-previousvalue')
-const displayValueH2 = document.querySelectorAll('[data-displayvalue')
+const equalButton = document.querySelector('[data-equal')
+const clearButton = document.querySelector('[data-clear')
+const previousValueTextElement = document.querySelector('[data-previousvalue')
+const displayValueTextElement = document.querySelector('[data-displayvalue')
+
+
+const calculator = new Calculator (previousValueTextElement, displayValueTextElement)
+
+numberButton.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNum(button.innerText)
+        calculator.updateDisplay()
+    })
+})
