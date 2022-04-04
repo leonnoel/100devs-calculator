48ca7de174fe64e9c97d382e70bdff7245f67b0c
admin 365 <ujbolivar@gmail.com>
2022-04-04 22:46:31 +0200

    got the calculator to log the value from each button pressed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-let button = document.querySelector('.button');
+// let button = document.querySelector('.button');
 
-function onScreen() {
-    document.querySelector('#screen').innerText = button.innerText
-}
-    button.addEventListener('click' , onScreen)
+// function onScreen() {
+//     document.querySelector('#screen').innerText = button.innerText
+// }
+//     button.addEventListener('click' , onScreen)
+
+const keys = document.querySelector('#btnPad');
+keys.addEventListener('click', event => {
+    const {target} = event;
+    const {value} = target;
+    let arr = []
+    let operators = ['.', 'x', '+', '-', '/', '=']
+    if (!target.matches('button')) {
+        return;
+    } else {
+        console.log(value);
+    }
+})
