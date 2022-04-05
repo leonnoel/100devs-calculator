6412a3cd7ebae74f2b242391a3d5d328e479a3a5
admin 365 <ujbolivar@gmail.com>
2022-04-05 22:08:00 +0200

    added some more methods to the calculator object

--- js/app.js
+++ js/app.js
@@ -1,40 +1,52 @@
-// calculator object
-
-function Calculator(nums) {
-
-    displayText = '0',
-    prevTotal = null,
-    
-
-    this.add = function () {
-        console.log('adds');
-    }
-    this.subtract = function() {
-        console.log('subtracts');
-    }
-    this.multiply = function() {
-        console.log('multiply');
-    }
-    this.divide = function() {
-        console.log('divides');
-    }
-    this.equal = function() {
-        console.log('equals');
-    }
-}
-
 // setting up button presses and return to the screen
 
 const keys = document.querySelector('#btnPad');
 keys.addEventListener('click', event => {
-    const {target} = event;
-    const {value} = target;
-    let arr = []
-    let operators = ['.', 'x', '+', '-', '/', '=']
+    const { target } = event;
+    const { value } = target;
     if (!target.matches('button')) {
         return;
     } else {
-        console.log(value);
-        document.querySelector('#screen').innerText = value
+        calculator.parseInput(value)
     }
 })
+
+// calculator object
+
+const calculator = {
+
+    displayText: '0',
+    prevTotal: null,
+
+    parseInput(value) {
+        switch (value) {
+            case '=':
+                //calculate the answer
+                break;
+            case '.':
+                if (this.displayText == 0) {
+                    // pass '0.' into add text method
+                } else {
+                    //add value to text string
+                }
+                break;
+            default:
+                //add value to text string
+                break;
+        }
+    },
+
+    addText(value) {
+        if (this.displayText === '0') {
+            this.displayText = ''
+        } else if( this.prevTotal !== null ) {
+            this.displayText = this.prevTotal;
+            this.prevTotal = null;
+        }
+        if (/*invalid inputs*/) {
+
+        }
+        this.displayText += value
+        //output text to screen
+    }
+}
