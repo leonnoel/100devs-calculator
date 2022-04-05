5fe9200e9865f423a73d4c5e0825eaf96fcce499
CodingClu8 <95002463+CodingClu8@users.noreply.github.com>
2022-04-05 13:46:13 -0400

    Add files via upload
    MY calculator

---
+++ js.js
@@ -0,0 +1,38 @@
+let display =document.getElementById('display');
+
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case'C':
+                display.innerText = '';
+                break;
+
+                case'←':
+                if(display.innerText){
+                display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+                   case'=':
+                   try{
+                    display.innerText = eval(display.innerText); 
+                   } catch {
+                       display.innerText ='Error!';
+                   }
+                   
+                   break;
+
+
+
+
+
+             default:
+                display.innerText += e.target.innerText;
+
+        }
+    });
+
+});
