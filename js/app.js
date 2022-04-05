20f2d8eb8ab1df9ab26c4d823dae299c11c66ae5
admin 365 <ujbolivar@gmail.com>
2022-04-05 21:36:47 +0200

    created the calculator object

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
-// let button = document.querySelector('.button');
+// calculator object
 
-// function onScreen() {
-//     document.querySelector('#screen').innerText = button.innerText
-// }
-//     button.addEventListener('click' , onScreen)
+function Calculator(nums) {
+
+    displayText = '0',
+    prevTotal = null,
+    
+
+    this.add = function () {
+        console.log('adds');
+    }
+    this.subtract = function() {
+        console.log('subtracts');
+    }
+    this.multiply = function() {
+        console.log('multiply');
+    }
+    this.divide = function() {
+        console.log('divides');
+    }
+    this.equal = function() {
+        console.log('equals');
+    }
+}
+
+// setting up button presses and return to the screen
 
 const keys = document.querySelector('#btnPad');
 keys.addEventListener('click', event => {
