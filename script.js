33057f0f4798ab7a8d67b3c7d0dfd88f5399d4d4
Derek Drackley <derekdrackley@gmail.com>
2022-04-06 02:01:13 -0500

    Allowed standalone values to save and overwrite previous calculations; documentation added

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+/*
+  Program by TribalK
+*/
 let Calculator = class {
+  //Constructor function
   constructor() {
-    this.total = 0;
-    this.preOpr = 0;
-    this.postOpr = 0;
-    this.strAns = "0";
-    this.strUpperDisp = "";
-    this.lastOpr = "";
-    this.newPass = true;
-    this.postSet = false;
+    this.total = 0;         //total of calculation
+    this.preOpr = 0;        //first numeric value in each calculation
+    this.postOpr = 0;       //second numeric value in each calculation
+    this.strAns = "0";      //screen to display current total
+    this.strUpperDisp = ""; //screen to display upper calculations
+    this.lastOpr = "";      //last operator set (held when setting an equals or arithmetic after setting the postOpr value)
+    this.newPass = true;    //awaiting a new action to commence on the calculator
+    this.postSet = false;   //determines if the postOpr value had been set to do calculations
   }
 
+  //result all values to default
   clearScreen() {
     this.total = 0;
     this.preOpr = 0;
@@ -21,12 +26,15 @@ let Calculator = class {
     this.postSet = false;
   }
 
+  //Setting display for equals sign getting clicked
   setEquals(total, context) {
     this.total = total;
 
+    //We want to avoid a situation such as "0 = 0 =", only one equals sign operator should be present
     if(this.lastOpr === context) {
       this.strUpperDisp = `${this.total} ${this.lastOpr}`;
     } else {
+      //ex: "2 + 3 ="
       this.strUpperDisp = `${this.preOpr} ${this.lastOpr} ${this.postOpr} ${context}`;
     }
 
@@ -39,9 +47,9 @@ let Calculator = class {
     this.postSet = false;
   }
 
+  //Determine which values and operators we want to set before we calculate arithmetic
   preSetCalculation(context) {
     //Not yet ready to change total, prevent doubling by spam clicking operators.
-
     if(this.newPass) {
       this.strUpperDisp = this.total.toString() + context;
       this.lastOpr = context;
@@ -55,7 +63,7 @@ let Calculator = class {
         this.postSet = true;
     }
   }
-
+  //Calculate based on operator
   calculate() {
     if(this.postSet) {
       switch(this.lastOpr) {
@@ -75,13 +83,14 @@ let Calculator = class {
           return this.preOpr / this.postOpr;
           break;
         }
+        //The user replaces the existing value with another number entirely.
         case '=': {
           return this.postOpr;
           break;
         }
       }
     }
-
+    //Not ready to calculate anything, return the pre-saved value
     return this.preOpr;
   }
 
@@ -91,8 +100,10 @@ let Calculator = class {
       return;
     }
 
+    //Prepend 0 to string if nothing is present before decimal point.
     let symbol = (this.newPass && number == "." ? ("0"+number) : number);
 
+    //Either start writing to screen or add onto screen
     if(this.newPass) {
       this.strAns = symbol;
       this.newPass = false;
@@ -101,8 +112,9 @@ let Calculator = class {
     }
   }
 
+  //Set total fields after arithmetic calculation.
+  //Screen display changes based on total.
   setTotals(total, context) {
-    console.log(total);
     this.total = total;
     this.preOpr = total;
     this.postOpr = 0;
@@ -114,6 +126,7 @@ let Calculator = class {
     this.postSet = false;
   }
 
+  //Display changes made to the calculator screen after any button is selected.
   setDisplay() {
     const screen = document.querySelector('#stack');
     const answer = document.querySelector('#answer');
@@ -128,12 +141,14 @@ let Calculator = class {
   }
 }
 
+//Create new object
 const calculator = new Calculator();
 
-// const allButtons = Array.from(document.getElementsByClassName('button'));
-  const operButtons = Array.from(document.getElementsByClassName('operation'));
-  const numberButtons = Array.from(document.getElementsByClassName('number'));
-  const clearButton = document.getElementById('clear').addEventListener('click', function() {
+//Event Listeners
+const operButtons = Array.from(document.getElementsByClassName('operation'));
+const numberButtons = Array.from(document.getElementsByClassName('number'));
+
+const clearButton = document.getElementById('clear').addEventListener('click', function() {
   calculator.clearScreen();
   calculator.setDisplay();
 });
@@ -141,7 +156,6 @@ const calculator = new Calculator();
 const equalsButton = document.getElementById('equals').addEventListener('click', function() {
   calculator.preSetCalculation(this.textContent.trim());
   const retVal = calculator.calculate();
-  console.log(retVal);
   calculator.setEquals(retVal,this.textContent.trim());
   calculator.setDisplay();
 });
