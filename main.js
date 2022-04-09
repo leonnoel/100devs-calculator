3a47e446e92b77bfe85afb5947f5a04773204ca5
Nora Vargas <noravgs92@gmail.com>
2022-04-08 23:39:26 -0500

    updated html,css and js files

--- main.js
+++ main.js
@@ -1,25 +1,85 @@
-let display = document.getElementById('display');
-let buttons = Array.from(document.getElementsByClassName('button'));
-buttons.map( button => {
- button.addEventListener('click', (e) => {
-     switch(e.target.innerText){
-         case '.':
-             display.innerText = '';
+//Required feautures for calculator
+//accept user inputs of numbers, operators, and other numbers
+//accept decimal numbers
+//store inputs
+//recognize inputs and perform calculations
+//return a result
+
+const keys = document.querySelector('.calculatorButtons');
+keys.addEventListener('click', event => {
+    const {target} = event;
+    const {value} = target;
+    if (!target.matches('button')) {
+     return;
+    } else {
+     calculator.parseInput(value)
+     // console.log(`thing is ${event}`)
+    }
+})
+
+const calculator = {
+     displayText: '0',
+     prevTotal: null,
+
+     parseInput(value) {
+      //have any of special buttons
+          switch (value) {
+            case '=' :
+             //calculate the answer
+             this.calcAnswer(this.displayText)
              break;
-         case '=':
-             try{
-                 display.innerText = eval(display.innerText);
-             } catch {
-                 display.innerText = "Error"
-             }
-             break;
-         case '←':
-             if (display.innerText){
-                display.innerText = display.innerText.slice(0, -1);
-             }
-             break;
-         default:
-             display.innerText += e.target.innerText;
+             case 'AC':
+              //clear screen and store value
+              this.clearAll()
+              break;
+              case '.':
+                if (this.displayText == 0) {
+                 //pass 0. into add text method
+                 this.addText('0.')
+                } else {
+                 //add value to text string
+                 this.addText(value)
+                }
+                break;
+                default:
+                 //add value to text string
+                 this.addText(value)
+                 break;
+               
+                }
+                },
+      
+       addText(value) {
+        if (this.displayText === '0') {
+         this.displayText = ''
+     }else if (this.prevTotal !== null) {
+          this.displayText = this.prevTotal
+          this.prevTotal = null
      }
- });
-});
+       if (isNaN(+(value)) && isNaN(+(this.displayText.slice(-1)))) {
+         
+          return;
+         
+       }
+       this.displayText += value
+       this.outputText(this.displayText)
+     },
+
+     outputText(text) {
+      document.querySelector('.display').value = text
+     },
+     calcAnswer(equation) {
+        let result = Function("return " + equation)()
+        this.outputText(result)
+     },
+     clearAll() {
+      this.displayText = '0',
+      this.prevTotal = null,
+      this.outputText(this.displayText)
+     }
+   
+    }
+    
+   
+
+
