499a22cc13c2fe6047a1b8db03b8d90748658832
Al.exe <alEXE-tech@protonmail.com>
2022-04-08 22:32:52 -0700

    Added preview display for current number being calculated above the main output; added to hold the numbers and operands; made progress on calculations logic; reorganized object to have the inner calculation methods and buffer properties to be private members; added a clear method as well as a method to clear all of the buffers on a double click; planning to add basic functionality to all of the remaining buttons next.

--- calc/js/main.js
+++ calc/js/main.js
@@ -1,8 +1,4 @@
 
-const SYMBOLS = ['+', '-', '/', '*', '='];
-const NUMBERS = ['-9','-8', '-7', '-6', '-5', '-4', '-3', '-2', '1', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const CLICKCOLOR = '#1e90ff';
-const NOCLICKCOLOR = '#fff';
 
 // constants for numbers and symbols first
 /* 
@@ -10,94 +6,114 @@ const NOCLICKCOLOR = '#fff';
 */
 
 //Constructor function for all Calculators
-function Calculator(pressedButton, total, currentValue, lastValue, buttons, display) {
-
-  //declared properties
-  this.pressedButton = pressedButton;
-  this.total = total;
-  this.currentValue = currentValue;
-  this.lastValue = lastValue;
+function Calculator(display = 0, preview = 0) {
+  //declared public properties
   this.display = display;
+  this.preview = preview;
+
+
+  /*
+  * Private Members (inner workings)
+  */
+  // local variables
+  const symbols = ['+', '-', '/', '*'];
+  const numbers = ['-9','-8', '-7', '-6', '-5', '-4', '-3', '-2', '1', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+  const clickColor = '#1e90ff';
+  const noClickColor = '#fff';
+
+  // buffers
+  let numBuffer1;   // holds first number
+  let symbolBuffer; // holds current arithmetic operation
+  let numBuffer2;   // holds second number
+  let clearFlag = 0;
 
   // arithmetic methods (remember to insert html later)
-  this.add = num => console.log(this.total += num);
-  this.subtract = num => console.log(this.total -= num);
-   this.multiply = num => console.log(this.total *= num);
-  this.divide = num => console.log(this.total /= num);
-
-  this.compute = function (symbol, num) {
+  add = _ => Number(numBuffer1) + Number(numBuffer2);
+  subtract = _ => Number(numBuffer1) - Number(numBuffer2);
+  multiply = _ => Number(numBuffer1) * Number(numBuffer2);
+  divide = _ => Number(numBuffer1) / Number(numBuffer2);
+  compute = (symbol) => {
     switch(symbol) {
       case '+':
-      return this.add(num);
+      return  add();
       case '-':
-      return this.subtract(num);
+      return  subtract();
       case '*':
-      return this.multiply(num);
+      return  multiply();
       case '/':
-      return this.divide(num);
+      return divide();
     }
   }
 
 
-  // fancy button methods
-   this.clear = _ => this.display = '0';
-  this.flipSign = _ => (this.total > 0) ? this.total -= (this.total * 2) : this.total += (this.total * 2);
-
-
-  /*
-  Button clicking logic, will send this to the prototype once finished
+  /* 
+  * Public methods
   */
-  
 
-  // Still WORKING ON ALL THIS
+  // clear button methods
+   this.clearDisplay = () => {
+    this.display = '0';
+    clearFlag++;
+  }
+
+  this.clearBuffers = () => {
+    numBuffer1 = undefined;
+    symbolBuffer = undefined;
+    numBuffer2 = undefined;
+    this.preview = 0;
+    clearFlag = 0;
+  }
+
+  // flipSign methods (needs to be integrated with number buffers
+  // this.flipSign = _ => (this.total > 0) ? this.total -= (this.total * 2) : this.total += (this.total * 2);
+
+
+  // The button clicking logic
+  // Still working but much progress has been made
   this.buttonClick = function(button) {
-    this.currentValue = button;
-
-    // Display 0 correctly until a number is given
-    if(this.display == 0 && NUMBERS.includes(this.currentValue)){
-      this.display = this.currentValue;
-      this.lastValue = this.currentValue;
+    // if the button clicked is an operand but the first number buffer is empty, reset the symbol buffer
+    if(symbols.includes(button) && !(numBuffer1)) {
+      symbolBuffer = undefined;
+    } 
+    // if the button clicked is a number but the operand hasn't been chosen, add number to the first buffer
+    else if(numbers.includes(button) && !(symbolBuffer)) {
+      numBuffer1 ? numBuffer1 += button : numBuffer1 = button;
+      this.preview = numBuffer1;
     }
-
-    // if the last value was a number and the current value is a number, append the current value to the display;
-    else if(NUMBERS.includes(this.lastValue) && NUMBERS.includes(this.currentValue)){
-      this.display += `${this.currentValue}`; // add to the large number
-      this.lastValue = this.currentValue;
+    // if button clicked is an operand and the firstNumber buffer is filled; reassign the symbol buffer 
+    else if(symbols.includes(button) && (numBuffer1)) {
+      symbolBuffer = button;
+      this.display = numBuffer1;
     }
-
-    // if the last value was a number and the current value is a symbol
-    else if(NUMBERS.includes(this.lastValue) && SYMBOLS.includes(this.currentValue)) {
-      // do stuff
-      this.total = Number(this.display);
-      this.lastValue = this.currentValue;
+    // if the button clicked is a number and the symbol buffer is filled AND the first number buffer is filled 
+    // add the button to the second number buffer
+    else if(numbers.includes(button) && symbolBuffer && numBuffer1) {
+      numBuffer2 ? numBuffer2 += button : numBuffer2 = button;
+      this.preview = numBuffer2;
     }
-
-    // if the last value was a symbol and the current value is a number, it's finally time to do some math
-    else if(SYMBOLS.includes(this.lastValue) && NUMBERS.includes(this.currentValue)) {
-      this.total = Number(this.display);
-      let currentNumber = Number(this.currentValue);
-      this.compute(this.lastValue, currentNumber);
-      this.display = this.total;
-      this.lastValue = this.currentValue;
-      this.currentValue = 0;
+    // if the button clicked was equal sign '=' call the compute private method and clear buffers
+    else if(button == '='){
+      this.display = symbolBuffer ? compute(symbolBuffer) : compute('+');
+      this.display = (this.display % 2 < 1) && (this.display % 2 != 0) ? this.display.toFixed(2) : this.display;
+      this.preview = this.display;
     }
-    // if  the last value was a symbol, and the CURRENT value is a symbol just change the operation and DON'T output!
-    else if(SYMBOLS.includes(this.lastValue) && SYMBOLS.includes(this.currentValue)) {
-      this.lastValue = this.currentValue;
+    else if(button === 'ac'){
+      clearFlag === 0 ? this.clearDisplay() : this.clearBuffers(); // must double click to clear buffers
     }
   }
 }
 
 
+
 const calc = new Calculator();
 const output = document.getElementById('output');
+const preview = document.getElementById('preview');
 const buttons = document.querySelectorAll('.btn');
 
-calc.display = '0';
-calc.currentValue = '0';
 buttons.forEach(btn => {
   btn.firstChild.addEventListener('click', function() {
     calc.buttonClick(btn.firstChild.id);
+    preview.innerHTML = calc.preview;
     output.innerHTML = calc.display;
   });
 });
