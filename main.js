581940ea0f36ab89ecdf5e2ed034d80d9c46ae23
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-10 10:10:07 +0200

    Commit additional methods

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const calculator = {
+
 initialize() {
   const buttonSet = Array.from(document.querySelectorAll(".calc-button"));
   buttonSet.forEach(button => button.addEventListener("click", this.updateArray));
@@ -6,14 +7,19 @@ initialize() {
   console.log("Calculator is running");
 },
 
-updateArray() {
+updateArray(click) {
+  probArr.push(click.target.value);
+  document.querySelector(".display-main").innerText = probArr.join("");
   console.log("Array is updated")
 },
 
 evaluateProblem() {
+  document.querySelector(".display-main").innerText = eval(probArr.join(""));
+  document.querySelector(".display-secondary").innerText = probArr.join("");
   console.log("Problem is evaluated")
 },
 
 };
 
+const probArr = [];
 calculator.initialize();
