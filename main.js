b4cdc5a9ca3072c124a5a95868fca11c58824db1
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-10 11:16:30 +0200

    Add proper indentation

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 const calculator = {
 
-initialize() {
-  const buttonSet = Array.from(document.querySelectorAll(".calc-button"));
-  buttonSet.forEach(button => button.addEventListener("click", this.updateArray));
-  document.querySelector(".equals").addEventListener("click", this.evaluateProblem);
-  console.log("Calculator is running");
-},
+  initialize() {
+    const buttonSet = Array.from(document.querySelectorAll(".calc-button"));
+    buttonSet.forEach(button => button.addEventListener("click", this.updateArray));
+    document.querySelector(".equals").addEventListener("click", this.evaluateProblem);
+    console.log("Calculator is running");
+  },
 
-updateArray(click) {
-  probArr.push(click.target.value);
-  document.querySelector(".display-main").innerText = probArr.join("");
-  console.log("Array is updated")
-},
+  updateArray(click) {
+    probArr.push(click.target.value);
+    document.querySelector(".display-main").innerText = probArr.join("");
+    console.log("Array is updated")
+  },
 
-evaluateProblem() {
-  let solution = eval(probArr.join(""));
-  document.querySelector(".display-main").innerText = solution;
-  document.querySelector(".display-secondary").innerText = probArr.join("");
-  probArr = [solution];
-  console.log("Problem is evaluated")
-},
+  evaluateProblem() {
+    let solution = eval(probArr.join(""));
+    document.querySelector(".display-main").innerText = solution;
+    document.querySelector(".display-secondary").innerText = probArr.join("");
+    probArr = [solution];
+    console.log("Problem is evaluated")
+  },
 
 };
 
