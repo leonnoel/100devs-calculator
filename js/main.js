1378e2ddfb179bfbd730bc6e3dfc0875bf120c7d
Tiago Cruz <sathoskath@gmail.com>
2022-04-10 12:41:36 -0300

    Added new feature when equal button clicked perform last operation repeteadly

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 const input = document.querySelector('#calc-screen')
 
-// document.querySelector('.seven').addEventListener('click', function() {
-//     document.getElementById('calc-screen').value += 7
-// })
-
 const numPad = [...document.querySelectorAll('.calc-button')].map(el => {return {value: el.value, text: el.innerText}});
+let counter = 0;
 
 function Calculator(buttons) {
+    this.displayResult = 0;
+    this.storedOperator;
+    this.secondStoredNumber;
+
     this.createButtonInteraction = function() {
         let operatorsArr = ['/', 'x', '+', '-']
-
+        
         buttons.forEach(element => {
             // If it is the reset button do nothing. I tried return document.querySelector('.reset'.addEventListener('click', this.calculate(input.value)))
             // but I believe doesn't work to call the this.calculate from inside the constructor. Not sure tho, instead I just did it outside of the constructor.
@@ -22,7 +23,7 @@ function Calculator(buttons) {
             // after the operator, so later easier to split the string into an array of 3 elements [n, operator, n]
             if (operatorsArr.includes(element.text)) {
                 return document.querySelector(`.${element.value}`).addEventListener('click', function() {
-
+                    counter = 0;
                     // If the user tries to add more than one operator then shows error message.
                     if (operatorsArr.includes(input.value.split(' ')[1])) {
                         alert('Only one operator allowed')
@@ -35,7 +36,7 @@ function Calculator(buttons) {
             // If it's the dot button, does a different logic and avoid more than one dot per number
             if (element.value === 'dot') {
                 return document.querySelector(`.${element.value}`).addEventListener('click', function() {
-
+                    if (counter > 0) {return}
                     // Throw alert error if user tries to place more than one dot on the first number
                     if (!input.value.split('').includes(' ')) {
                         input.value.includes('.') ? alert('Invalid decimal. Please use only one . per number') : input.value += element.text;
@@ -48,36 +49,49 @@ function Calculator(buttons) {
             } 
 
             return document.querySelector(`.${element.value}`).addEventListener('click', function() {
-                    input.value += element.text;
+                if (counter > 0) {return}
+                input.value += element.text;
             })
         })
     }
 
     this.calculate = function(str) {
         let expressionArray = str.split(' ');
-        let result = 0;
+
+        if (expressionArray[1] === undefined) {
+            if (expressionArray[0] === '' || this.storedOperator === undefined) {return};
+            expressionArray[1] = this.storedOperator;
+            expressionArray[2] = this.secondStoredNumber;
+        }
+
+        this.storedOperator = expressionArray[1];
+        this.secondStoredNumber = expressionArray[2];
+
+
+        counter = 1;
         switch (expressionArray[1]) {
             case '+':
-                result = +expressionArray[0] + +expressionArray[2];
+                this.displayResult = +expressionArray[0] + +expressionArray[2];
                 break;
 
             case '-':
-                result = +expressionArray[0] - +expressionArray[2];
+                this.displayResult = +expressionArray[0] - +expressionArray[2];
                 break;
 
             case 'x':
-                result = +expressionArray[0] * +expressionArray[2];
+                this.displayResult = +expressionArray[0] * +expressionArray[2];
             break;
 
             case '/':
-                result = +expressionArray[0] / +expressionArray[2];
+                this.displayResult = +expressionArray[0] / +expressionArray[2];
             break;
         }
         
-        input.value = result;
+        input.value = this.displayResult;
     }
 
     this.reset = function() {
+        counter = 0;
         input.value = '';
     }
 }
