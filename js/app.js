3d7e6bc7008ce176586c55ae21ad1a116fa288ae
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-04-16 05:11:11 +0600

    done

--- js/app.js
+++ js/app.js
@@ -3,30 +3,60 @@ class Calculator {
         this.displayValues = document.getElementById('display')
         this.button = document.querySelectorAll('.btn');
         this.btnVal(this)
-        this.calcResult
+        this.arr = []
 
     }
 
     btnVal(val) {
         this.button.forEach(el =>
             el.addEventListener('click', function(e) {
-                val.displayValues.innerText += e.currentTarget.value
+                let values = e.currentTarget.value
+                switch (values) {
+                    case '+':
+                        val.arr.push(Number(val.displayValues.innerText))
+                        val.arr.push(values)
+                        val.displayValues.innerText = ''
+                        break
+                    case '-':
+                        val.arr.push(Number(val.displayValues.innerText))
+                        val.arr.push(values)
+                        val.displayValues.innerText = ''
+                        break
+                    case '*':
+                        val.arr.push(Number(val.displayValues.innerText))
+                        val.arr.push(values)
+                        val.displayValues.innerText = ''
+                        break
+                    case '/':
+                        val.arr.push(Number(val.displayValues.innerText))
+                        val.arr.push(values)
+                        val.displayValues.innerText = ''
+                        break
+                    case '=':
+                        val.arr.push(Number(val.displayValues.innerText))
+                        val.displayValues.innerText = ''
+                        break
+                    default:
+                        val.displayValues.innerText += e.currentTarget.value
 
+                }
             })
         )
     }
 
     calcResult() {
-        return this.displayValues.innerText = eval(this.displayValues.innerText)
+        let cal = ''
+        this.arr.filter((x, i) => x !== 0 ? cal += x : false)
+        this.arr = []
+        this.displayValues.innerText = eval(cal)
 
     }
+    resetBtn() { this.displayValues.innerText = '' }
 
 }
 
 let calculator = new Calculator
 
 
-document.querySelector("#evaluate").addEventListener('click', function() {
-    calculator.calcResult()
-
-})
+document.querySelector("#evaluate").addEventListener('click', function() { calculator.calcResult() })
+document.querySelector("#reset").addEventListener('click', function() { calculator.resetBtn() })
