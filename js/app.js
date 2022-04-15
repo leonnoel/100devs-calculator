531226c8fb5661d06022008682376f73f3d44460
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-04-15 21:39:28 +0600

    moved function prototype inside class

--- js/app.js
+++ js/app.js
@@ -2,14 +2,22 @@ class Calculator {
     constructor() {
         this.displayValues = document.getElementById('display')
         this.button = document.querySelectorAll('.btn');
+        this.btnVal(this)
+        this.calcResult
 
-        let parentThis = this
+    }
 
+    btnVal(val) {
         this.button.forEach(el =>
             el.addEventListener('click', function(e) {
-                parentThis.displayValues.innerText += e.currentTarget.value
+                val.displayValues.innerText += e.currentTarget.value
+
             })
         )
+    }
+
+    calcResult() {
+        return this.displayValues.innerText = eval(this.displayValues.innerText)
 
     }
 
@@ -18,8 +26,7 @@ class Calculator {
 let calculator = new Calculator
 
 
-Calculator.prototype.calcResult = function() {
-    return this.displayValues.innerText = eval(this.displayValues.innerText)
-}
+document.querySelector("#evaluate").addEventListener('click', function() {
+    calculator.calcResult()
 
-document.querySelector("#evaluate").addEventListener('click', function() { calculator.calcResult() })
+})
