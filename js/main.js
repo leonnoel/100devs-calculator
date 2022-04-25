85481381a32ea97c51c1e954dd2b4b67ef0cfb6e
Taha Shaikh <taha_shaikh@sfu.ca>
2022-04-25 14:55:41 -0700

    updated calculator

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
-let buttonInput = document.querySelectorAll(".button")
-let answer = document.querySelector(".answer")
+let buttons = Array.from(document.getElementsByClassName('button'));
+let display = document.querySelector(".answer")
 
-buttonInput.forEach(function (btn) {
-    btn.addEventListener('click', function () {
-        answer.appendNumber(button.innerText)
-        answer.updateDisplay()
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
     });
 });
+
 
 
