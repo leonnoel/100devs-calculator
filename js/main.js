1d18602be08f6e86b81bc5970feb4c255064baa2
Jessica Fuller <jessicanfuller808@gmail.com>
2022-04-27 17:44:34 -0700

    working calculator

--- js/main.js
+++ js/main.js
@@ -1,38 +1,97 @@
-/**
- * Want to make a function where if you click a button, that button's value is pushed to the display.
- * When the equal sign is pressed, it calculates the values loaded in the display, then displays the total.
- */
+// a class will allow storage of all information typed in the display
+class Calculator {
+    //contains all inputs and functions for calculator
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        //gives us a way to set these text elements inside our calculator class
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
 
-// let total = document.getElementById('total');
-// let button7 = document.getElementById('seven');
-// let displayTotal = "";
+    //makes a clean slate any times the calculator is created
+    clear () {
+        this.currentOperand = "";
+        this.previousOperand = "";
+        this.operation = undefined;
+    }
 
+    // adds number to the screen every time it's clicked. passes number user selected.
+    appendNumber(number) {
+        //this allows only one period to be displayed
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
 
-// function updateTotal(btn) {
-//     numTotal = Number(total.innerText) + Number(btn.innerText);
-//     total.innerText = numTotal;
-//     displayTotal += numTotal;
-//     // return total += btn;
-// }
+    // happens anytime a user clicks on an operation. passes operation user selected.
+    chooseOperation(operation) {
+        if(this.currentOperand === "") return;
+        if (this.previousOperand !== "") {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
 
-// button7.addEventListener('click', updateTotal(button7));
+    // takes values inside of calculator and creates a single value to display on output.
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            case 'x':
+                computation = prev * current;
+                break;
+            default: 
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
 
-
-
-let calc = {
-    "total": 0,
-    "0": 0,
-    "1": 1,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "/": () => {a / b},
-    "x": () => {a * b},
-    "+": () => {a + b},
-    "-": () => {a - b},
+    // update the values inside of our output.
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand;
+        this.previousOperandTextElement.innerText = this.previousOperand;
+    }
 }
+
+// Variables of the different buttons
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+//actually creates the calculator
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () =>  {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () =>  {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
