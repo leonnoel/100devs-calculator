895409c8c1dfbea9440fcb66ba9825e62536e3c2
Boobeh123 <boobeh321@gmail.com>
2022-04-27 16:53:21 -1000

    Event listener logs button values to console

--- main.js
+++ main.js
@@ -7,3 +7,15 @@
 
 // Listen for clicks anywhere inside the container
 const keys = document.querySelector('.calculator-buttons');
+    // Function called on click
+    keys.addEventListener('click', event => {
+
+        // Checks event object. If target property was clicked from button element
+        if (event.target.matches('button')) {
+
+            // Then logs value from event object's target property
+            console.log(event.target.value);
+        } else {
+            return;
+        }
+})
