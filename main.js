ab2710298319d9dc1cc0b6695eb0227eb0d070e5
Boobeh123 <boobeh321@gmail.com>
2022-04-30 13:44:23 -1000

    Add more pseudocode to object

--- main.js
+++ main.js
@@ -28,29 +28,46 @@ numberPad.addEventListener('click', event => {
 }
 })
 
+// Object created with literal notation
 const calculator = {
-    // Text that shows up when calculator is first opened
+    // Property 1 displays text on calculator-screen
     displayText: '0',
+    // Property 2 previousTotal becomes Start of next operation
+    previousTotal: null,
 
-    // Value passed in from event listener
-    parseInput(value) {
+    /* Method 1
+    parseInput function takes in event.target.value from event listener 
+        Receive good input by sanitizing (if/else)
+            Routes input through default or cases (switch decision tree)
+    */
+    // Start of parseInput
+    parseInput(value) 
+    {
         // Checks displayText value and set displayText to blank
-        if (this.displayText === '0') {
+        if (this.displayText === '0') 
+        {
             this.displayText = ''
         }
+
         // Have any of the operators, equals, decimals, or AC been clicked?
-        switch(value) {
-            // Calculate the answer
+        switch(value) 
+        {
             case '=':
+                // If equals sign is clicked, calculate the answer
                 break;
 
-            // Clear the screen and store values
             case 'AC':
+                // If all-clear is clicked, Clear the screen and store values
                 break;
 
-            // 
+                /* If decimal is clicked, two situations: 
+                1) The decimal is the first character in the string -> Zero displays ahead of the decimal (example: 0.1)
+                2) The first character in the string is not 0 -> Zero disappears and updates to 1 (example: 1.5)
+                */
             case '.':
                 break;
         }
+
     }
+    // End of parseInput
 }
