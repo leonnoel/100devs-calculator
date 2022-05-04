375c10114705c5e924b2589e60a36923600a254e
FoleyDom <dfoleydev@gmail.com>
2022-05-04 13:53:20 -0400

    Initial commit

--- js/main.js
+++ js/main.js
@@ -14,13 +14,41 @@
 
 const keys = document.querySelector('.calculator-buttons')
 keys.addEventListener('click', (event) => {
-  const { target } = event;
-  const { value } = target;
-  if (!target.matches('button')) {
-    return
-  } else {
-    //pass to parse method
-    console.log(`this is ${event}`)
-  }
+	const { target } = event
+	const { value } = target
+	if (!target.matches('button')) {
+		return
+	} else {
+		calculator.parseInput(value)
+	}
 })
 
+const calculator = {
+	displayText: '0',
+	prevTotal: null,
+
+	parseInput(value) {
+		if (this.displayText === '0') {
+			this.displayText = ''
+		}
+		//have any special buttons been clicked
+		switch (value) {
+			case '=':
+				//calculate the answer
+				break
+			case 'AC':
+				//clear screem
+				break
+			case '.':
+				if (this.displayText == 0) {
+					//pass '0.' into add text method
+				} else {
+					//add value to text string
+				}
+		}
+
+		//addText(value){
+			
+		}
+	}
+}
