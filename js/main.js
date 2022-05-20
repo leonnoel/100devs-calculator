ebcfa6feb36ee5fb92584784ea48bc07bf034b9f
neuroleptique <corn.xiao@gmail.com>
2022-05-20 00:09:55 -0400

    create calculator layout with html and css

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+//when clicking buttons (eventlistener), the numbers will apprear on the screen located at current
+//numbers added up until an operator is clicked
+//then 
+//when clicking "=" sign, the result generated and shows on the screen 
+
+
+
+
+class Calculator {
+    constructor(){
+
+    }
+    
+}
