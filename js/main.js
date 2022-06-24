90a791347cc70343527bf4ac71f55162f4a4cc05
ryfurness <ryan.f.allard@gmail.com>
2022-06-24 09:17:14 -0400

    added number entry ability

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const add = (function () {
+    let counter = 0;
+    return function () {counter += 1; return counter}
+  })();
+  
+  add();
+
+const calc = (function (){
+    let calculation = {
+        firstNumber : "",
+        secondNumber : "",
+        operation : "",
+        result : ""
+    }
+    return {
+        putNum: function (num){
+            console.log(num)
+            if (calculation.firstNumber === "") {
+                calculation.firstNumber = num
+            }else {
+                calculation.firstNumber += num
+            }
+            document.querySelector(".screen").innerText = calculation.firstNumber
+        }
+
+        // function updateScreen(type){
+        //     switch (type){
+        //         case "putNum":
+        //             document.querySelector(".screen").textContent = ""
+        // }
+    }
+})()
+
+document.querySelectorAll(".number").forEach(numBtn=>{
+    console.log(numBtn.innerHTML)
+    numBtn.addEventListener("click", function(){calc.putNum(numBtn.innerHTML)})
+})
