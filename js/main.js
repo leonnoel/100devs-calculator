6c2f06c67a30986dfb04c08686c03c97267663d3
Christina Milner <christina@christina-codes.dev>
2022-08-10 19:47:03 +0100

    Class implementation started but pending values logic properly wrecked

--- js/main.js
+++ js/main.js
@@ -1,32 +1,137 @@
-// Variables that will be used over and over
+// DOM elements
 
-let operator;
-let buffer;
-let pending;
-let lastButtonPressed;
+const display = document.querySelector('.display')
+const numberButtons = document.querySelectorAll('.num')
+const operatorButtons = document.querySelectorAll('.operator')
+const equalsButton = document.querySelector('.equals') 
 
-const display = document.querySelector('.display');
-display.textContent = '0';
+// Calculator class
 
-// Basic maths functions
+ class Calculator {
+        constructor(number) {
+            this.displayValue = number
+            this.value = number
+            this.num = null
+            this.operator = null
+        }
+    add() {
+        this.getValue()
+        this.value += this.num
+    }
+    subtract() {
+        this.getValue()
+        this.value -= this.num
+    }
+    multiply() {
+        this.getValue()
+        this.value *= this.num
+    }
+    divide() {
+        this.getValue()
+        if (this.num == 0) {
+            this.displayValue = "ERROR!"
+            this.showValue()
+            this.displayValue = 0}
+        this.value /= this.num
+    }
+    getValue() {
+        this.value = this.displayValue
+    }
+    showValue() {
+        display.textContent = this.displayValue
+    }
+    addNumber(number) {
+        if (this.displayValue && number !== '.') {
+            this.displayValue = Number(String(this.displayValue) + String(number))
+            this.showValue()
+        } else if (number == '.') {
+            if (display.textContent.indexOf('.') >= 0) {
+                return
+            }
+            this.displayValue = String(this.displayValue) + "."
+            this.showValue()
+        }
+        else {
+            this.displayValue = number
+            this.showValue()
+        }
+    }
+    addOperator(op) {
+        if (!this.op || !this.num) {
+            this.operator = op
+            this.value = this.displayValue
+            this.displayValue = ''
+        }
+        else {
+            this.calculate()
+            this.operator = op
+        }
+    }
+    calculate() {
+        if (this.operator == '+') {
+            this.add()
+        }
+        if (this.operator == '-') {
+            this.subtract()
+        }
+        if (this.operator == 'divide') {
+            this.divide()
+        }
+        if (this.operator == 'x') {
+            this.multiply()
+        }
+        this.num = null
+        this.operator = null
+        this.showValue()
+    }
+    equalsFunction() {
+    }     
+ }
 
-function add(a, b) {
-    return a + b;
+// Initialize a new calculator with value 0 on page load 
+
+ let calc = new Calculator(0)
+ calc.showValue()
+
+// Event listeners to make button clicks work
+
+ numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calc.addNumber(button.textContent);
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calc.addOperator(button.id);
+    });
+})
+
+equalsButton.addEventListener('click', () => {
+    calc.equalsFunction();       
+});
+
+function equalsFunction() {
+    if (operator && buffer && !pending && lastButtonPressed !== 'op') {
+        let num = makeNumber(display.textContent);
+        let result = operate(operator, buffer, num);
+        display.textContent = result;
+        buffer = null;
+        operator = null;
+        pending = 1;        // Pending even though no operation is pending, because if user
+    }                       // enters new numbers here, it should do new calculation, not concatenate
+    lastButtonPressed = 'eq';
 }
 
-function multiply(a, b) {
-    return a * b;
+// Helper functions
+
+function makeNumber(content) {
+    if (content.indexOf('.') >= 0) {
+        return parseFloat(content);
+    }
+    else {return parseInt(content);}
 }
 
-function subtract(a, b) {
-    return a - b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-// Operate function
 
 function operate(operator, num1, num2) {
     if (operator == '+') {
@@ -46,48 +151,7 @@ function operate(operator, num1, num2) {
     }
 }
 
-// Make numbers buttons work
 
-const numberButtons = document.querySelectorAll('.num');
-
-numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        displayNumber(button.textContent);
-    });
-});
-
-function displayNumber(number) {
-    if (display.textContent == '0' || display.textContent == 'Error!') {
-        clearDisplay();
-    }
-    if (number == '.' && display.textContent.indexOf('.') >= 0) {  // Make sure no decimal points can be entered if there's already one there
-        return;
-    }
-
-    if (pending) {
-        if (operator) {
-            buffer = makeNumber(display.textContent);}
-        clearDisplay();
-        pending = null;
-    }
-    if (display.textContent.length < 12) {
-        if (number == '.' && display.textContent == '') {display.textContent += 0;}
-        display.textContent += number;
-        pending = null;
-    }
-    lastButtonPressed = 'num';
-}
-
-
-// Make operators work
-
-const operatorButtons = document.querySelectorAll('.operator');
-
-operatorButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        dealWithOperator(button.id);
-    });
-});
 
 function dealWithOperator(button) {
     let num = (display.textContent == 'Error!') ? 0 : makeNumber(display.textContent); 
@@ -104,107 +168,23 @@ function dealWithOperator(button) {
     lastButtonPressed = 'op'
 }
 
+
+/*
+
 // Equals sign button
 
-const equalsButton = document.querySelector('.equals');
-equalsButton.addEventListener('click', () => {
-    equalsFunction();       // needs to be separate named function so I can call it on keyboard input
-});
 
-function equalsFunction() {
-    if (operator && buffer && !pending && lastButtonPressed !== 'op') {
-        let num = makeNumber(display.textContent);
-        let result = operate(operator, buffer, num);
-        display.textContent = result;
-        buffer = null;
-        operator = null;
-        pending = 1;        // Pending even though no operation is pending, because if user
-    }                       // enters new numbers here, it should do new calculation, not concatenate
-    lastButtonPressed = 'eq';
-}
-
-// In place operators
-
-const inPlaceButtons = document.querySelectorAll('.inPlaceOp')
-inPlaceButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        operateInPlace(button.id);
-    });
-});
-
-function operateInPlace(action) {
-    let num = (display.textContent == 'Error!') ? 0 : makeNumber(display.textContent);
-    let disp = display.textContent;
-    if (action == 'percent') {
-        display.textContent = num / 100;
-    }
-    else if (action == 'backspace') {
-        if (display.textContent.length == 1) {
-            display.textContent = 0;
-        }
-        else {
-            display.textContent = disp.slice(0, -1);
-        }        
-    }
-    else if (action == 'invert') {
-        if (num !== 0) {
-            display.textContent = 1 / num;
-        } else {display.textContent = 'Error!'}
-    }
-    else if (action == 'square') {
-        display.textContent = num ** 2;
-    }
-    else if (action == 'sqrt') {
-        display.textContent = Math.sqrt(num);
-    }
-    else if (action == 'neg') {
-        display.textContent = - num;
-    }
-    lastButtonPressed = 'oip';
-}
-
-// CE and CE buttons
-
-const deleteButtons = document.querySelectorAll('.delete');
-deleteButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        deleteThings(button);
-    });
-});
-
-function deleteThings(button) {
-    display.textContent = '0';
-    if (button.id == 'C') {
-        buffer = null;
-        operator = null;
-        pending = null;
-    }
-}
-
-// Helper functions
-
-function makeNumber(content) {
-    if (content.indexOf('.') >= 0) {
-        return parseFloat(content);
-    }
-    else {return parseInt(content);}
-}
-
-// Clear the display
-
-function clearDisplay() {
-    display.textContent = '';
-}
 
 // Keyboard support
-
+*/
+/*
 document.addEventListener('keydown', (event) => {
     const keyName = event.key;
     if (!isNaN(keyName) || keyName == '.') {
         displayNumber(keyName);
     }
     if (keyName == 'Backspace') {
-        operateInPlace('backspace');       /* I should've made the button IDs the same as the keyboard input names, but OH WELL */
+        operateInPlace('backspace');       
     }
     if (keyName == 'Enter') {
         equalsFunction();
@@ -218,7 +198,9 @@ document.addEventListener('keydown', (event) => {
     if (keyName == '*') {
         dealWithOperator('x');
     }
-    if (keyName == ',') {            /* otherwise numpad decimal point won't work for ppl using German keyboard layout like me */
+    if (keyName == ',') {            
         displayNumber('.');
     }
 });
+
+*/
