884079ff036d3be4b20a5a8c9f1a6b20df078267
Christina Milner <christina@christina-codes.dev>
2022-08-10 20:16:33 +0100

    Mostly fixed but behaves funny when chaining operators instead of using equals

--- js/main.js
+++ js/main.js
@@ -9,65 +9,64 @@ const equalsButton = document.querySelector('.equals')
 
  class Calculator {
         constructor(number) {
-            this.displayValue = number
             this.value = number
             this.num = null
             this.operator = null
         }
     add() {
-        this.getValue()
         this.value += this.num
     }
     subtract() {
-        this.getValue()
         this.value -= this.num
     }
     multiply() {
-        this.getValue()
         this.value *= this.num
     }
     divide() {
-        this.getValue()
         if (this.num == 0) {
-            this.displayValue = "ERROR!"
-            this.showValue()
-            this.displayValue = 0}
+            display.innerText = "ERROR!"
+        }
         this.value /= this.num
     }
-    getValue() {
-        this.value = this.displayValue
-    }
+
     showValue() {
-        display.textContent = this.displayValue
+        display.textContent = this.value
     }
     addNumber(number) {
-        if (this.displayValue && number !== '.') {
-            this.displayValue = Number(String(this.displayValue) + String(number))
-            this.showValue()
+        if (display.innerText == '0' || display.innerText == 'ERROR!' || display.innerText == ' ' || this.operator) {display.innerText = ''}
+        if (number !== '.') {
+            display.innerText += number
         } else if (number == '.') {
             if (display.textContent.indexOf('.') >= 0) {
                 return
             }
-            this.displayValue = String(this.displayValue) + "."
-            this.showValue()
+            display.innerText += number
+        }
+        alert(`Value: ${this.value}\nNumber: ${this.num}\nOperator: ${this.operator}`)
+    }
+    getValueFromDisplay() {
+        if (!this.value) {
+            this.value = Number(display.innerText)
         }
         else {
-            this.displayValue = number
-            this.showValue()
+            this.num = Number(display.innerText)
         }
     }
+
     addOperator(op) {
         if (!this.op || !this.num) {
+            this.getValueFromDisplay()
             this.operator = op
-            this.value = this.displayValue
-            this.displayValue = ''
         }
         else {
+            this.getValueFromDisplay()
             this.calculate()
             this.operator = op
         }
+        alert(`Value: ${this.value}\nNumber: ${this.num}\nOperator: ${this.operator}`)
     }
     calculate() {
+        this.getValueFromDisplay()
         if (this.operator == '+') {
             this.add()
         }
@@ -85,6 +84,8 @@ const equalsButton = document.querySelector('.equals')
         this.showValue()
     }
     equalsFunction() {
+        this.calculate()
+        alert(`Value: ${this.value}\nNumber: ${this.num}\nOperator: ${this.operator}`)
     }     
  }
 
@@ -111,72 +112,8 @@ equalsButton.addEventListener('click', () => {
     calc.equalsFunction();       
 });
 
-function equalsFunction() {
-    if (operator && buffer && !pending && lastButtonPressed !== 'op') {
-        let num = makeNumber(display.textContent);
-        let result = operate(operator, buffer, num);
-        display.textContent = result;
-        buffer = null;
-        operator = null;
-        pending = 1;        // Pending even though no operation is pending, because if user
-    }                       // enters new numbers here, it should do new calculation, not concatenate
-    lastButtonPressed = 'eq';
-}
-
-// Helper functions
-
-function makeNumber(content) {
-    if (content.indexOf('.') >= 0) {
-        return parseFloat(content);
-    }
-    else {return parseInt(content);}
-}
-
-
-function operate(operator, num1, num2) {
-    if (operator == '+') {
-        return add(num1, num2);
-    }
-    else if (operator == '-') {
-        return subtract(num1, num2);
-    }
-    else if (operator == 'divide') {
-        if (num2 !== 0) {
-            return divide(num1, num2);    
-        }
-        return 'Error!'
-    }
-    else if (operator == 'x') {
-        return multiply(num1, num2);
-    }
-}
-
-
-
-function dealWithOperator(button) {
-    let num = (display.textContent == 'Error!') ? 0 : makeNumber(display.textContent); 
-    if (!buffer) {
-        buffer = num;
-    }
-    else if (lastButtonPressed !== 'op') {
-        let result = operate(operator, buffer, num);
-        display.textContent = result;
-        buffer = result;
-    }
-    pending = 1;
-    operator = button;
-    lastButtonPressed = 'op'
-}
-
-
-/*
-
-// Equals sign button
-
-
-
 // Keyboard support
-*/
+
 /*
 document.addEventListener('keydown', (event) => {
     const keyName = event.key;
