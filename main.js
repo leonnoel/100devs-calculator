2c183fc598dfc66f7cf535539a6bd79a9ca054f2
Philip Siu <philipsiu@hotmail.com>
2022-08-23 11:17:15 -0700

    add methods to calculator constructor

--- main.js
+++ main.js
@@ -8,6 +8,41 @@ keys.addEventListener('click', event => {
     if (!target.matches('button')) {
         return;
     } else {
-        console.log(event);
+        let calc = new Calculator();
+        calc.parseInput(value);
     }
-})
+});
+
+class Calculator {
+    constructor() {
+        this.displayText: '0';
+        this.previousTotal: null;
+    }
+    parseInput(value) {
+        switch(value) {
+            case '=':
+                // calculate answer
+                break;
+            case 'AC':
+                // clear screen and stored values
+                break;
+            case '.':
+                if (this.displayText === 0) {
+                    // pass string of 0. into add text method
+                } else {
+                    // add value to text string
+                }
+                break;
+            default:
+                // add value to text string
+        }
+    }
+    addText(value) {
+        if (this.displayText === '0') {
+            this.displayText = '';
+        } else if (this.prevTotal) {
+            this.displayText = this.prevTotal;
+            this.prevTotal = null;
+        }
+    }
+}
