96247438cd5874ccaa1d508b5e6f0cc77ab64642
Philip Siu <philipsiu@hotmail.com>
2022-08-24 11:13:41 -0700

    add calculate and clearScreen methods

--- main.js
+++ main.js
@@ -1,5 +1,59 @@
+class Calculator {
+    constructor() {
+        this.displayText = '0';
+        this.previousTotal = null;
+    }
+    parseInput(value) {
+        switch(value) {
+            case '=':
+                this.calculate(this.displayText);
+                break;
+            case 'AC':
+                this.clearScreen();
+                break;
+            case '.':
+                if (this.displayText === 0) {
+                    this.addText('0.');
+                } else {
+                    this.addText(value);
+                }
+                break;
+            default:
+                this.addText(value);
+                break;
+        }
+    }
+    addText(value) {
+        if (this.displayText === '0') {
+            this.displayText = '';
+        } else if (this.previousTotal) {
+            this.displayText = this.previousTotal;
+            this.previousTotal = null;
+        }
+        if (isNaN(+(value)) && isNaN(+(this.displayText))) {
+            if (isNaN(this.displayText.slice(-1)))
+                return;
+        }
+        this.displayText += value;
+        this.outputText(this.displayText);
+    }
+    outputText(text) {
+        document.querySelector('.calc-screen').value = text;
+    }
+    calculate(equation) {
+        let result = Function('return ' + equation)();
+        this.outputText(result);
+    }
+    clearScreen() {
+        this.displayText = '0';
+        this.previousTotal = null;
+        this.outputText(this.displayText);
+    }
+}
+
 // get user input and ensure it's valid
 const keys = document.querySelector('.calc-buttons');
+const calc = new Calculator();
 keys.addEventListener('click', event => {
     // shorthand for object deconstruction
     // same as event.target or event.target.value
@@ -8,41 +62,6 @@ keys.addEventListener('click', event => {
     if (!target.matches('button')) {
         return;
     } else {
-        let calc = new Calculator();
         calc.parseInput(value);
     }
 });
-
-class Calculator {
-    constructor() {
-        this.displayText: '0';
-        this.previousTotal: null;
-    }
-    parseInput(value) {
-        switch(value) {
-            case '=':
-                // calculate answer
-                break;
-            case 'AC':
-                // clear screen and stored values
-                break;
-            case '.':
-                if (this.displayText === 0) {
-                    // pass string of 0. into add text method
-                } else {
-                    // add value to text string
-                }
-                break;
-            default:
-                // add value to text string
-        }
-    }
-    addText(value) {
-        if (this.displayText === '0') {
-            this.displayText = '';
-        } else if (this.prevTotal) {
-            this.displayText = this.prevTotal;
-            this.prevTotal = null;
-        }
-    }
-}
