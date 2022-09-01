1e12268539118009cea8e742635320b3e1eb0c9d
James <jameskvo.ii@gmail.com>
2022-08-31 20:48:42 -0700

    keyboard fucntionality and fixed second operator issue

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ const displayNum = document.querySelector(".display");
 var firstOperand = "";
 var secondOperand = "";
 var operator = null;
+var prevOperator = null;
 var operatorPressed = false;
+var waitingForOperand = false;
 
 // TO DO:
 // FIX DECIMAL -> OPERATOR -> NUM = NaN
-// add key presses
-// using keyboard inputs after clearing is bugged
-// cannot use a different operator eg. 1+1-1=3, should equal 2
+// set . to 0 if no other numbers are pressed
 
 buttons.forEach((button) => {
     button.addEventListener("click", function () {
-        // number button is pressed
+        // reset font size
+        document.querySelector(".display").style.fontSize = "";
+        if (displayNum.innerText === "Cannot divide by 0") {
+            clearDisplay();
+            clearVariables();
+        }
+
         if (!button.classList.contains("operator") && !button.classList.contains("function")) {
             if (button.getAttribute("data-num") === ".") {
                 if (displayNum.textContent.includes(".")){
@@ -29,28 +35,39 @@ buttons.forEach((button) => {
 
         // operator button is pressed
         } else if (button.classList.contains("operator")) {
-            if (operatorPressed && button.innerText !== "=") {
+            if (operatorPressed && button.innerText !== "=" && waitingForOperand !== true) {
+                operator = button.innerText;
+                waitingForOperand = true;
+                console.log("waitingForOperand now set to true");
                 return;
+            }
+            // after we hit = we need to store the next operand to be used
+            if (waitingForOperand) {
+                secondOperand = displayNum.innerText;
+                console.log(`new secondOperand: ${secondOperand}`);
             }
             if (firstOperand === "" && !isNaN(parseFloat(displayNum.innerText))) {
                 firstOperand = displayNum.innerText;
             } else if (operator) {
                 // for repeated = button presses
                 const result = secondOperand === "" ? operate(parseFloat(firstOperand), parseFloat(displayNum.innerText), operator) : operate(parseFloat(firstOperand), parseFloat(secondOperand), operator);
-                var prevOperator = operator;
+                prevOperator = operator;
                 secondOperand = secondOperand !== "" ? secondOperand : displayNum.innerText;
-                console.log(displayNum.innerText);
-                displayNum.innerText = String(result);       
+                displayNum.innerText = String(result);  
             }
 
             // console.log(operator);
             // console.log("display: " + parseFloat(displayNum.innerText)); 
+            // operator = button.innerText !== "=" ? button.innerText : "";
             operator = button.innerText;
+            console.log(`updating operator to ${operator}`);
             operatorPressed = true;
             firstOperand = displayNum.innerText;
 
+            // BUG IS HERE, I'M SETTING OPERATOR TO PREVIOUS OPERATOR SO IT WILL NEVER TAKE THE NEW OPERATOR VALUE
             // for repeated = button presses
             if (operator === "=") {
+                waitingForOperand = false;
                 operator = prevOperator;
             } else {
                 // operator = button.innerText
@@ -62,13 +79,20 @@ buttons.forEach((button) => {
         } else if (button.classList.contains("function") && button.innerText == "AC") {
             // const display = document.querySelector(".display");
             // display.textContent = "0";
-            console.log("kek");
             clearDisplay();
-            firstOperand = "";
-            operator = null;
-            secondOperand = "";
-            operatorPressed = false;
+            clearVariables();
+            // firstOperand = "";
+            // operator = null;
+            // secondOperand = "";
+            // operatorPressed = false;
         }
+    // console.log(`firstOperand: ${firstOperand}`);
+    // console.log(`secondOperand: ${secondOperand}`);
+    // console.log(`operator: ${operator}`);
+    // console.log(`operatorPressed: ${operatorPressed}`);
+    // console.log(`prevOperator: ${prevOperator}`);
+    // console.log(`waitingForOperand: ${waitingForOperand}`);
+    // console.log("------------------------------------------------");
     });
 });
 
@@ -81,11 +105,14 @@ document.addEventListener("keydown", function(event) {
     if (!isNaN(keyPress)) {
         document.querySelector(`[data-num="${keyPress}"]`).click();
     }
-    else if (document.querySelector(`[data-ops="${keyPress}"]`)){
+    else if (document.querySelector(`[data-ops="${keyPress}"]`)) {
         document.querySelector(`[data-ops="${keyPress}"]`).click();
     }
     else if (keyPress == "Enter") {
-        document.querySelector(`[data-ops="="]`).click();
+       document.querySelector('[data-ops="="]').click();
+    }
+    else if (keyPress == "Escape") {
+        document.querySelector('[data-ops="clear"]').click();
     }
 });
 /*
@@ -106,6 +133,10 @@ function operate(firstNum, secondNum, operatorVal) {
     } else if (operatorVal === "x") {
         return firstNum * secondNum;
     } else if (operatorVal === "/") {
+        if (secondNum === 0) {
+            document.querySelector(".display").style.fontSize = "21px";
+            return "Cannot divide by 0"
+        }
         return firstNum / secondNum;
     }
     // return secondOperand;
@@ -116,3 +147,11 @@ function clearDisplay() {
     display.textContent = "0";
 }
 
+function clearVariables() {
+    firstOperand = "";
+    secondOperand = "";
+    operator = null;
+    prevOperator = null;
+    operatorPressed = false;
+    waitingForOperand = false;
+}
