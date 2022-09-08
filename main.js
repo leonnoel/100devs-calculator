fd33e17589ef764107b3f48564cdfc3c8cf8e69a
Jericho <jericho.ramos108@gmail.com>
2022-09-08 15:23:59 +0800

    Operations now working

--- main.js
+++ main.js
@@ -1,77 +1,72 @@
-const calculator = document.querySelector(".calculator")
-const clearBtn = calculator.querySelector(".calculator__btn--clear")
-const deleteBtn = calculator.querySelector(".calculator__btn--delete")
-const calcAnswer = calculator.querySelector(".calculator__answer")
-const numberBtns = calculator.querySelectorAll(".calculator__btn--num")
-const pointBtn = calculator.querySelector(".calculator__btn--point")
-const opBtns = calculator.querySelectorAll(".calculator__btn--operator")
+const calculator = document.querySelector(".calculator");
+const clearBtn = calculator.querySelector(".calculator__btn--clear");
+const deleteBtn = calculator.querySelector(".calculator__btn--delete");
+const calcAnswer = calculator.querySelector(".calculator__answer");
+const numberBtns = calculator.querySelectorAll(".calculator__btn--num");
+const pointBtn = calculator.querySelector(".calculator__btn--point");
+const opBtns = calculator.querySelectorAll(".calculator__btn--operator");
 
-let previousOperand = '', currentOperand = '', currentOperator;
+let previousOperand = "",
+  currentOperand = "",
+  currentOperator,
+  runningTotal = 0;
 
-numberBtns.forEach(numberBtn => {
-  numberBtn.addEventListener('click', () => {
-
+numberBtns.forEach((numberBtn) => {
+  numberBtn.addEventListener("click", () => {
     isOperatorBlank(numberBtn);
-  })
-})
+  });
+});
 
-opBtns.forEach(opBtn => {
-  opBtn.addEventListener('click', () => {
+opBtns.forEach((opBtn) => {
+  opBtn.addEventListener("click", () => {
     appendOperator(opBtn);
-  })
-})
+  });
+});
 
 function isOperatorBlank(btn) {
   if (!currentOperator) {
     previousOperand += btn.innerText;
+    calcAnswer.innerText = previousOperand;
   } else {
     currentOperand += btn.innerText;
+    calcAnswer.innerText += currentOperand;
   }
-  console.log(previousOperand, currentOperator, currentOperand)
 }
 
-
 function appendOperator(btn) {
-  if (!currentOperator || currentOperator.length !== 1) {
-    currentOperator = btn.innerText
-  } else {
-    calculateAnswer(currentOperator)
+  if (previousOperand && currentOperator && currentOperand) {
+    previousOperand = Number(previousOperand);
+    currentOperand = Number(currentOperand);
+    calcAnswer.innerText += currentOperator;
   }
+  currentOperator = btn.innerText;
+  calcAnswer.innerText += currentOperator;
 }
 
 function calculateAnswer(operatorStr) {
-  let answer;
-  switch (operatorStr) {
-    case '+':
-      answer = Number(previousOperand) + Number(currentOperand);
-      break;
-    case 'x':
-      answer = Number(previousOperand) * Number(currentOperand);
-      break;
-    case '/':
-      answer = Number(previousOperand) / Number(currentOperand);
-      break;
-    case '-':
-      answer = Number(previousOperand) - Number(currentOperand);
-      break;
+  if (currentOperand) {
+    switch (operatorStr) {
+      case "+":
+        runningTotal = previousOperand + currentOperand;
+        previousOperand = runningTotal;
+        currentOperand = "";
+        break;
+      case "x":
+        runningTotal = previousOperand * currentOperand;
+        previousOperand = runningTotal;
+        currentOperand = "";
+        break;
+      case "/":
+        runningTotal = previousOperand / currentOperand;
+        previousOperand = runningTotal;
+        currentOperand = "";
+        break;
+      case "-":
+        runningTotal = previousOperand - currentOperand;
+        previousOperand = runningTotal;
+        currentOperand = "";
+        break;
+    }
   }
-  // console.log(answer)
-  return answer;
+  return runningTotal;
 }
-
-
-// console.log(calculateAnswer)
-
-// function calculateAnswer(operatorStr) {
-//   switch (operatorStr) {
-//     case '+':
-//       let answer = previousOperand + currentOperand
-//   }
-// }
-
-// function addOperator() {
-
-// }
-
-
-    // console.dir(numberBtn.innerText)
