fb2e3fb7d1f9d7b22316f8809f21f7e1d81b633f
Tom <81436713+CurtainsAreBetter@users.noreply.github.com>
2022-09-16 13:06:14 -0400

    Finish draft of Calculator object

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ function Calculator() {
     this.rightSide = '';
     this.total = 0;
     this.leftSideFocus = true;
+    this._currentOperator = '';
 
     this.reset = () => {
         this.leftSide = 0;
@@ -11,10 +12,10 @@ function Calculator() {
     }
     
     // Math
-    this.add = () => this.total = +this.leftSide + +this.rightSide;
-    this.subtract = () => this.total = +this.leftSide - +this.rightSide;
-    this.multiply = () => this.total = +this.leftSide * +this.rightSide;
-    this.divide = () => this.total = +this.leftSide / +this.rightSide;
+    this.add = () => +this.leftSide + +this.rightSide;
+    this.subtract = () => +this.leftSide - +this.rightSide;
+    this.multiply = () => +this.leftSide * +this.rightSide;
+    this.divide = () => +this.leftSide / +this.rightSide;
 
     // data entry
     //numbers
@@ -46,4 +47,53 @@ function Calculator() {
         }
     }
 
+    //calc stuff
+    this.equate = () => {
+        // verify ready to equate
+        if (!(this.leftSide && this.rightSide && this._currentOperator))
+
+        // get operator
+        // run operation and set total
+        switch (this._currentOperator) {
+            case 'add': return this.add();
+            case 'subtract': return this.subtract();
+            case 'divide': return this.divide();
+            case 'multiply': return this.multiply();            
+        }
+    }
+
+    // set operator
+    this.setOperator = (operation) => {
+        // make sure operation is valid
+        if (!['add', 'divide', 'subtract', 'multiply'].includes(operation)) {
+            console.error('Not a valid operator');
+        }
+        // check if let side has content
+        if (this.leftSide) {
+            // if left side has content but right side does not, just update the operator and change focus
+            if (!this.rightSide) {
+                console.log('operator updated');
+                this._currentOperator = operation;
+                this.leftSideFocus = false;
+            } else {
+                // right side has content
+                // right side can only have content if an operator is set (not implemented yet)
+                // if so, equate, set answer to left side, clear right side, set new operator, 
+                const ans = this.equate();
+                this.leftSide = ans.toString();
+                this.rightSide = '';
+                this._currentOperator = operation;
+            }
+        } else {
+            // if not, do nothing
+            return;
+        }
+    }
+
 }
+
+
+
+
+
+
