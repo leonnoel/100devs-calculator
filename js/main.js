73e9371c73b9f1f54ae8ebf3734cbb6b6e44d1bb
Hristijan <hristijan93@hotmail.com>
2022-09-23 23:34:11 +0200

    completed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-
-
-
 function Calculator() {
     let result = document.querySelector(`.result`);
     let buttons = Array.from(document.querySelectorAll(`.key`));
 
     buttons.map(el => el.addEventListener(`click`, (button) => {
-        result.innerText += button.target.innerText;        
+        if (button.target.innerText  === '=') {
+            result.innerText = eval(result.innerText);
+        } else result.innerText += button.target.innerText;
     }));
     
 
 }
 
 let calc = new Calculator();
-// console.log(calc.sum());
