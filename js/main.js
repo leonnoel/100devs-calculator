a8fe95429e241ce9738b9a0f26e3edd1c3142737
Hristijan <hristijan93@hotmail.com>
2022-09-23 14:14:27 +0200

    some functionallity added when button is clicked it appears on the calculator display

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 
+
+
+function Calculator() {
+    let result = document.querySelector(`.result`);
+    let buttons = Array.from(document.querySelectorAll(`.key`));
+
+    buttons.map(el => el.addEventListener(`click`, (button) => {
+        result.innerText += button.target.innerText;        
+    }));
+    
+
+}
+
+let calc = new Calculator();
+// console.log(calc.sum());
