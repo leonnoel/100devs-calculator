04f94f4274bef23f01af4ae3ea7afe24e56d7724
Biggnness <Bjoseph3262@gmail.com>
2022-10-16 01:03:27 -0400

    added js changes
    added hmtl css and js

---
+++ mainjs/main.js
@@ -0,0 +1,120 @@
+const numButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operatoon]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = docuemtn.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+class Calcultor {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+}
+clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+}
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay(this.currentOperandTextElement.innerText = this.currentOperand)
+    })
+})
+appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+}
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+chooseOperation(operation) {
+    if (this.currentOperand === '') return
+    if (this.previousOperand !== '') {
+        this.compute()
+    }
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ''
+}
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+compute() {
+    let computation
+    const prev = parseFlaot(this.previousOperand)
+    const current = pareFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+        case '+':
+            computation = prev + current
+            break
+        case '-':
+            computation = prev - current
+        case '*':
+            computation = prev * current
+        case '÷':
+            computation = prev / current
+            break
+        default:
+            return
+
+    }
+    this.currentOperand = computation
+    this.operation = undefinded
+    this.previousOperand = ''
+}
+allClearButton.addEventLitener('click', button => {
+    calculator.clear()
+    calcualator.updateDisplay()
+})
+delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
+}
+deleteButton.addEventLitener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
+updateDisplay () {
+    this.currentOperandTextElement.innerText =
+    this.getDisplayNumber(this.currentOperand)
+    if (this.operation != null) {
+        this.previousOperandTextElement.innerText = 
+        `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    }else {
+        this.previousOperandTextElement.innerText = ''
+    }
+}
+getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if (isNaN(integerDigits)) {
+      integerDisplay = ''
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } else {
+      return integerDisplay
+    }
+  }
+
+  getDisplayNumber(number) {
+    if (decimalDigits != null) {
+          return `${integerDisplay}.${decimalDigits}`
+        } else {
+          return integerDisplay
+        }
+      }
