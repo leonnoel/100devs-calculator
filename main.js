e9ffc5094e765324793a709dadd9aa66bca66d70
Hector Torres <h.s.torres96@gmail.com>
2023-02-09 00:42:04 -0800

    add basic functionality, needs OOP implementation

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 //CREATING THE BUTTONS
 const buttonsSection = document.querySelector('#buttonsSection');
 
-const buttonText = {
-  seven: 7,
-  eight: 8,
-  nine: 9,
-  divide: '/',
-  four: 4,
-  five: 5,
-  six: 6,
-  multiply: 'x',
-  one: 1,
-  two: 2,
-  three: 3,
-  add: '+',
-  zero: 0,
-  decimal: '.',
-  evaluate: '=',
-  subtract: '-',
-};
-for (let key in buttonText) {
+const buttonText = [
+  7,
+  8,
+  9,
+  '/',
+  4,
+  5,
+  6,
+  'x',
+  1,
+  2,
+  3,
+  '+',
+  0,
+  '.',
+  '=',
+  '-',
+  'AC',
+];
+for (let i = 0; i < buttonText.length; i++) {
   const button = document.createElement('span');
   button.classList.add('round');
-  button.textContent = `${buttonText[key]}`;
+  button.textContent = buttonText[i];
   buttonsSection.appendChild(button);
+  if (i == buttonText.length - 1) {
+    button.classList.add('reset');
+  }
 }
 
 //BUTTON FUNCTIONALITY OF DISPLAYING ON SCREEN
@@ -35,19 +39,88 @@ for (let button of buttons) {
   button.addEventListener('click', (e) => {
     if (e.target.textContent == '=') {
       expression = screen.textContent;
-      console.log(evalExpression(expression));
+      if (expression.includes('+')) {
+        console.log(expression);
+        console.log(evalAddition(expression));
+        screen.textContent = operators['+'](...evalAddition(expression));
+      } else if (expression.includes('x')) {
+        evalMultiplication(expression);
+      } else if (expression.includes('-')) {
+        evalSubtraction(expression);
+      } else {
+        evalDivision(expression);
+      }
+    } else if (e.target.textContent == 'AC') {
+      screen.textContent = '';
     } else {
-      screen.textContent += button.textContent;
+      screen.textContent += `${button.textContent} `;
     }
   });
 }
 
-function evalExpression(str) {
-  return str.split('').map((value) => {
-    if (Number(value)) {
-      return Number(value);
-    } else {
-      return value;
-    }
-  });
+//EVALUATION OBJECTS
+function evalAddition(str) {
+  return str
+    .trim()
+    .split('+')
+    .map((value) => {
+      if (Number(value)) {
+        return Number(value);
+      } else {
+        return Number(value.replaceAll(' ', ''));
+      }
+    });
 }
+function evalMultiplication(str) {
+  return str
+    .trim()
+    .split('x')
+    .map((value) => {
+      if (Number(value)) {
+        return Number(value);
+      } else {
+        return value;
+      }
+    });
+}
+function evalSubtraction(str) {
+  return str
+    .trim()
+    .split('-')
+    .map((value) => {
+      if (Number(value)) {
+        return Number(value);
+      } else {
+        return value;
+      }
+    });
+}
+function evalDivision(str) {
+  return str
+    .trim()
+    .split('/')
+    .map((value) => {
+      if (Number(value)) {
+        return Number(value);
+      } else {
+        return value;
+      }
+    });
+}
+
+const operators = {
+  x: function (num1, num2) {
+    return Number(num1) * Number(num2);
+  },
+  '-': function (num1, num2) {
+    return num1 - num2;
+  },
+  '/': function (num1, num2) {
+    return num1 / num2;
+  },
+  '+': function (num1, num2) {
+    return Number(num1) + Number(num2);
+  },
+};
+
+//
