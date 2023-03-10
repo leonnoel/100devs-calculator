7a3f2be4f4dd5ed67de944c8c3e3ae17d1ff08ba
Gxgxn <gdsinghb99@gmail.com>
2023-03-09 23:31:33 -0400

    finished the calculator UI

---
+++ script.js
@@ -0,0 +1,13 @@
+const blob = document.getElementById("blob");
+
+window.onpointermove = (event) => {
+  const { clientX, clientY } = event;
+
+  blob.animate(
+    {
+      left: `${clientX}px`,
+      top: `${clientY}px`,
+    },
+    { duration: 3000, fill: "forwards" }
+  );
+};
