fda9c951ed5095658dfe7cca4f3f831eae92d811
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-04-09 16:57:36 -0300

    Created displayFct()

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
+class Calculator {
+
+    constructor() {
+      this.result = 0;
+      this.numberButtons = document.querySelectorAll('.number')
+      this.display = document.querySelector('#display')
+    }
+
+    displayFct() {
+        this.numberButtons.forEach(number => {
+            number.addEventListener('click', () => {
+              display.value = Number(number.value);
+            });
+          })
+    }
+
+    add(num) {
+        this.result += num
+        return this.result
+    }
+
+    substract(num) {
+        this.result -= num
+        return this.result
+    } 
+
+    multiply(num) {
+        this.result *= num
+        return this.result
+    } 
+
+    divide(num) {
+        if (num === 0) {
+            throw new Error('Division by zero')
+        }
+        else {
+            this.result /= num
+            return this.result
+        }
+    }
+
+    getResult() {
+        return this.result
+    }
+
+}
+
+// Inicializo la calcu
+
+let calc = new Calculator()
+
+calc.displayFct()
 
