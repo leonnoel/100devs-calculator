4843d80331b1ab250f56afde3dbb29a280c3f17b
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-04-10 15:01:15 -0300

    finished main functionality with operationFct()

--- js/main.js
+++ js/main.js
@@ -3,15 +3,12 @@ class Calculator {
     constructor() {
       this.result = 0;
       this.numberButtons = document.querySelectorAll('.number')
+      this.operatorButtons = document.querySelectorAll('.operator')
       this.display = document.querySelector('#display')
-    }
-
-    displayFct() {
-        this.numberButtons.forEach(number => {
-            number.addEventListener('click', () => {
-              display.value = Number(number.value);
-            });
-          })
+      this.operatorValue = undefined
+      this.botonSuma = document.querySelector('#suma')
+      this.botonPunto = document.querySelector('#punto')
+      this.botonIgual = document.querySelector('#igual')
     }
 
     add(num) {
@@ -43,11 +40,63 @@ class Calculator {
         return this.result
     }
 
+    
+    displayFct() {
+        this.numberButtons.forEach(number => {
+            number.addEventListener('click', () => {
+              this.display.value = this.display.value + number.value;
+            });
+          })
+
+          this.botonPunto.addEventListener('click', () => {
+            if (!this.display.value.includes('.')){
+                this.display.value = this.display.value + this.botonPunto.value;
+            }
+        })
+    }
+
+
+    operationFct() {
+
+        // Primera instancia, se apreta el operador y guardamos ese numero en result y el operador en operatorValue
+        this.operatorButtons.forEach(operator => {
+            operator.addEventListener('click', () => {
+                this.result = Number(this.display.value)
+                this.operatorValue = operator.value
+                this.display.value = ''
+                console.log(`Result ${this.result}`)
+                console.log(`Operator ${this.operatorValue}`)
+            })
+        })
+
+        // Segunda instancia, el usuario tipea el segundo numero y presiona igual y eso da el resultado
+        this.botonIgual.addEventListener('click', () => {
+            let num = Number(this.display.value)
+            if (this.operatorValue === '+') {
+                this.add(num)
+                this.display.value = this.result
+            }
+            else if (this.operatorValue === '-') {
+                this.substract(num)
+                this.display.value = this.result
+            }
+            else if (this.operatorValue === 'x') {
+                this.multiply(num)
+                this.display.value = this.result
+            }
+            else if (this.operatorValue === '/') {
+                this.divide(num)
+                this.display.value = this.result
+            }
+               
+        } )
+    }   
+
 }
+
 
 // Inicializo la calcu
 
 let calc = new Calculator()
-
 calc.displayFct()
-
+calc.operationFct()
