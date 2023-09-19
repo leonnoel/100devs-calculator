0f709f22bd38d613a291f539bd5e5d0cef687e3f
Luka-DV <luka.guva@gmail.com>
2023-09-19 17:16:03 +0200

    add code for calc class syntax

--- js/main.js
+++ js/main.js
@@ -55,3 +55,67 @@ document.querySelector("#sum").addEventListener("click", calc.getResult)
 
 
 
+//with Classes:
+
+
+class CalculatorClass {
+
+    #display;
+    #reset = false;
+    #displayElement = document.querySelector(".display");
+
+    
+    #input = {
+        seven: "7",
+        eight: "8",
+        nine: "9",
+        divide: "/",
+        four: "4",
+        five: "5",
+        six: "6",
+        multiply: "*",
+        one: "1",
+        two: "2",
+        three: "3",
+        add: "+",
+        zero: "0",
+        point: ".",
+        subtract: "-",
+    };
+
+    buttonClick(id) {
+        if(this.#reset) {
+            this.#displayElement.innerText = "";
+        }
+        this.#display = this.#input[id]
+        this.#displayElement.innerText += this.#display;
+        this.#reset = false;
+    }
+    
+
+    getResult() {
+        try {
+            const result = new Function(`return ${this.#displayElement.innerText}`);
+            this.#displayElement.innerText = result();
+            this.#reset = true;
+        } catch {
+            this.#displayElement.innerText = "Error: Incorrect input"
+            this.#reset = true;
+        }
+        
+    }
+    
+
+}
+const calcClass = new CalculatorClass();
+
+document.querySelectorAll(".input").forEach(element => element.addEventListener("click", () => {
+    calcClass.buttonClick(element.id); // Pass the clicked element's id
+    })
+);
+
+document.querySelector("#sum").addEventListener("click", () => {
+    calcClass.getResult();
+});
+
+
