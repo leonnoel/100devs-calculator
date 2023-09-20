03e25f645b102234fec86ab358b5cc2501219cfd
Daniel Garcia <danvgar@gmail.com>
2023-09-20 03:44:05 -0700

    Finished
    Works pretty much exactly as you'd expect a calculator to work. Things to work on next - float errors in javascript.

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,12 +24,12 @@ function operate(input) {
         (input === '-') ||
         (input === '/') ||
         (input === '*') ||
-        (input === `**`) ||
+        (input === `^`) ||
         (screen[screen.length - 1] === '+') ||
         (screen[screen.length - 1] === '-') ||
         (screen[screen.length - 1] === '/') ||
         (screen[screen.length - 1] === '*') ||
-        (screen[screen.length - 1] === '**')
+        (screen[screen.length - 1] === '^')
     ) {
         screen.push(input)
     } else {
@@ -42,48 +42,70 @@ function operate(input) {
 
 //functions
 
+function eraseScreenAfterEnter() {
+    if(typeof screen[0] === 'number' && screen.length === 1) { 
+        // screen would only be a number and length of 1 if enter was pressed last. So if we just pressed enter and now we press a number, we want to assume the user is starting over again.
+        screen = []
+    }
+}
+
 //numbers
+document.querySelector('#id0').onclick = () => {
+    eraseScreenAfterEnter()
+    operate(`0`)
+    document.querySelector('#live-screen').innerText = screen.join(" ")
+}
+
 document.querySelector('#id1').onclick = () => {
+    eraseScreenAfterEnter()
     operate(`1`)
     document.querySelector('#live-screen').innerText = screen.join(" ")
 }
 
 document.querySelector('#id2').onclick = () => {
+    eraseScreenAfterEnter()
     operate(`2`)
     document.querySelector('#live-screen').innerText = screen.join(" ")
 }
 
 document.querySelector('#id3').onclick = () => {
+    eraseScreenAfterEnter()
     operate(`3`)
     document.querySelector('#live-screen').innerText = screen.join(" ")
 }
 
 document.querySelector('#id4').onclick = () => {
+    eraseScreenAfterEnter()
     operate(`4`)
     document.querySelector('#live-screen').innerText = screen.join(" ")
 }
 
 document.querySelector('#id5').onclick = () => {
+    eraseScreenAfterEnter()
     operate(`5`)
     document.querySelector('#live-screen').innerText = screen.join(" ")
 }
 
 document.querySelector('#id6').onclick = () => {
+    eraseScreenAfterEnter()
     operate(`6`)
     document.querySelector('#live-screen').innerText = screen.join(" ")
 }
 
 document.querySelector('#id7').onclick = () => {
+    eraseScreenAfterEnter()
     operate(`7`)
     document.querySelector('#live-screen').innerText = screen.join(" ")
 }
 
 document.querySelector('#id8').onclick = () => {
+    eraseScreenAfterEnter()
     operate(`8`)
     document.querySelector('#live-screen').innerText = screen.join(" ")
 }
 
 document.querySelector('#id9').onclick = () => {
+    eraseScreenAfterEnter()
     operate(`9`)
     document.querySelector('#live-screen').innerText = screen.join(" ")
 }
@@ -111,7 +133,7 @@ document.querySelector('#divide').onclick = () => {
 }
 
 document.querySelector('#power').onclick = () => {
-    operate(`**`)
+    operate(`^`)
     document.querySelector('#live-screen').innerText = screen.join(" ")
 }
 
@@ -123,14 +145,46 @@ document.querySelector('#dot').onclick = () => {
 
 // clear
 document.querySelector('#clear').onclick = () => {
-    screen = null
+    screen = []
     document.querySelector('#live-screen').innerText = screen
+    console.log(screen)
 }
 
 // enter
 document.querySelector('#enter').onclick = () => {
     //PEMDAS
+
     // Exponents
-    screen.indexOf(`**`)
-    console.log(screen.indexOf(`**`))
+    while (screen.includes(`^`)) {
+        let index = screen.indexOf(`^`)
+        screen[index] = Number(screen[index - 1]) ** Number(screen[index + 1])
+        screen.splice(index - 1, 1)
+        screen.splice(index, 1)
+    }
+
+    // Multiplcation and Division
+    while (screen.includes(`*`) || screen.includes(`/`)) {
+        let index = screen.findIndex(element => (element === `*`) || (element === `/`))
+        if (screen[index] === `*`) {
+            screen[index] = Number(screen[index - 1]) * Number(screen[index + 1])
+        } else if(screen[index] === `/`) {
+            screen[index] = Number(screen[index - 1]) / Number(screen[index + 1])
+        }
+        screen.splice(index - 1, 1)
+        screen.splice(index, 1)
+    }
+
+    // Addition and Subtraction
+    while (screen.includes(`+`) || screen.includes(`-`)) {
+        let index = screen.findIndex(element => (element === `+`) || (element === `-`))
+        if (screen[index] === `+`) {
+            screen[index] = Number(screen[index - 1]) + Number(screen[index + 1])
+        } else if(screen[index] === `-`) {
+            screen[index] = Number(screen[index - 1]) - Number(screen[index + 1])
+        }
+        screen.splice(index - 1, 1)
+        screen.splice(index, 1)
+    }
+    console.log(screen)
+    document.querySelector('#live-screen').innerText = screen
 }
