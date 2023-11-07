c935f7808abc417b5006ab2f1c3264fe9c232124
Tai Phan <ptai7722@gmail.com>
2023-11-07 16:04:37 -0600

    recreated the screen adding a div and adding a p to show the current expression

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function Calculator() {
+  //lets write some pseudocode to see what is required of us
+  //we must add any button clicked to the evaluation screen of the calculator.
+  //in the case the equal sign is pressed we need to evaluate the expression inside result, innerText
+  //in the case C(clear) is pressed we should delete the last character from the expression
+  //we need to grab all the operator buttons
+  //we need to create methods to add, subtract, divide, and multiply
+  this.add = function(a, b) {
+
+  }
+}
+
+const calculator = new Calculator();
