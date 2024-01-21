9b870e7eee8fdaf5af0aa1f52c7af9a9aeeb7128
Fazelhadi <salehzadehfazelhadi@gmail.com>
2024-01-21 09:06:31 +0430

    completed all js, css and html files

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 let calculator = {
   read() {
-
+    this.a = +prompt('Enter the first number?', '0')
+    this.b = +prompt('Enter the second number?', '0')
   },
 
   sum() {
-    let num1 = document.querySelector('input').value
+    return this.a + this.b
   },
 
   mul() {
-
+    return this.a * this.b
   },
 
   sub() {
-
+    return this.a - this.b
   },
 
   division() {
-
+    return this.a / this.b
   }
 }
 
@@ -24,12 +25,55 @@ let calculator = {
 document.querySelectorAll('li').forEach(function(li) {
   li.addEventListener('click', addToInput);
 });
-
 document.querySelector('.zeroMake').addEventListener('click', makeZero)
+document.querySelectorAll('.operator').forEach(function(operator) {
+  operator.addEventListener('click', doTheOperator)
+})
+
+
+function doTheOperator() {
+  let clickedSpan = event.target;
+  let spanText = clickedSpan.innerText;
+
+  // Read the current numbers
+  calculator.read();
+
+  let result;
+  if(spanText === '+') {
+    result = calculator.sum();
+  } else if(spanText === '-') {
+    result = calculator.sub();
+  } else if(spanText === '/') {
+    result = calculator.division();
+  } else {
+    result = calculator.mul();
+  }
+
+  // Update the input value with the result
+  let input = document.querySelector('input');
+  input.value = result;
+  input.classList.add('placeholder-style');
+
+  input.addEventListener('input', function() {
+    if (input.value !== String(result)) {
+      input.classList.remove('placeholder-style');
+    }
+  });
+}
+
 
 function makeZero() {
-  document.querySelector('input').value = 0
+  let input = document.querySelector('input');
+  input.value = 0;
+  input.classList.add('placeholder-style');
+
+  input.addEventListener('input', function() {
+    if (input.value !== '0') {
+      input.classList.remove('placeholder-style');
+    }
+  });
 }
+
 
 function addToInput(event) {
   let clickedLi = event.target;
