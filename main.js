5483b9147261173b49387bb953b29fa44305011b
tramirez30 <ramirez.tomas@yahoo.com>
2024-03-02 12:58:15 -0800

    Linking DOM elements to JS

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+// Button DOM //
+
+let output = document.querySelector("#output");
+
+let num_7 = document.querySelector("#btn-7");
+let num_8 = document.querySelector("#btn-8");
+let num_9 = document.querySelector("#btn-9");
+let addition = document.querySelector("#btn-plus");
+
+let num_4 = document.querySelector("#btn-4");
+let num_5 = document.querySelector("#btn-5");
+let num_6 = document.querySelector("#btn-96");
+let multiply = document.querySelector("#btn-mul");
+
+let num_1 = document.querySelector("#btn-1");
+let num_2 = document.querySelector("#btn-2");
+let num_3 = document.querySelector("#btn-3");
+let division = document.querySelector("#btn-div");
+
+let num_0 = document.querySelector("#btn-0");
+let decimalPoint = document.querySelector("#btn-dot");
+let equalSign = document.querySelector("#btn-equal");
+let subtraction = document.querySelector("#btn-sub");
