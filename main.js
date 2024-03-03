7c1a217e5a41311338842998aab18095e680c185
tramirez30 <ramirez.tomas@yahoo.com>
2024-03-03 11:34:51 -0800

    Figuring out OOP JS -- ran into issues

--- main.js
+++ main.js
@@ -1,23 +1,82 @@
+// let output = document.querySelector("#output");
+
+// let num_7 = document.querySelector("#btn-7");
+// let num_8 = document.querySelector("#btn-8");
+// let num_9 = document.querySelector("#btn-9");
+// let addition = document.querySelector("#btn-plus");
+
+// let num_4 = document.querySelector("#btn-4");
+// let num_5 = document.querySelector("#btn-5");
+// let num_6 = document.querySelector("#btn-96");
+// let multiply = document.querySelector("#btn-mul");
+
+// let num_1 = document.querySelector("#btn-1");
+// let num_2 = document.querySelector("#btn-2");
+// let num_3 = document.querySelector("#btn-3");
+// let division = document.querySelector("#btn-div");
+
+// let num_0 = document.querySelector("#btn-0");
+// let decimalPoint = document.querySelector("#btn-dot");
+// let equalSign = document.querySelector("#btn-equal");
+// let subtraction = document.querySelector("#btn-sub");
+
 // Button DOM //
 
-let output = document.querySelector("#output");
+// let Calculator = {
+//   output: document.querySelector("#output"),
+//   num_7: document.querySelector("#btn-7"),
+//   num_8: document.querySelector("#btn-8"),
+//   num_9: document.querySelector("#btn-9"),
+//   addition: document.querySelector("#btn-plus"),
 
-let num_7 = document.querySelector("#btn-7");
-let num_8 = document.querySelector("#btn-8");
-let num_9 = document.querySelector("#btn-9");
-let addition = document.querySelector("#btn-plus");
+//   num_4: document.querySelector("#btn-4"),
+//   num_5: document.querySelector("#btn-5"),
+//   num_6: document.querySelector("#btn-96"),
+//   multiply: document.querySelector("#btn-mul"),
 
-let num_4 = document.querySelector("#btn-4");
-let num_5 = document.querySelector("#btn-5");
-let num_6 = document.querySelector("#btn-96");
-let multiply = document.querySelector("#btn-mul");
+//   num_1: document.querySelector("#btn-1"),
+//   num_2: document.querySelector("#btn-2"),
+//   num_3: document.querySelector("#btn-3"),
+//   division: document.querySelector("#btn-div"),
 
-let num_1 = document.querySelector("#btn-1");
-let num_2 = document.querySelector("#btn-2");
-let num_3 = document.querySelector("#btn-3");
-let division = document.querySelector("#btn-div");
+//   num_0: document.querySelector("#btn-0"),
+//   decimalPoint: document.querySelector("#btn-dot"),
+//   equalSign: document.querySelector("#btn-equal"),
+//   subtraction: document.querySelector("#btn-sub"),
 
-let num_0 = document.querySelector("#btn-0");
-let decimalPoint = document.querySelector("#btn-dot");
-let equalSign = document.querySelector("#btn-equal");
-let subtraction = document.querySelector("#btn-sub");
+//   sum() {},
+
+//   multiply() {},
+
+//   subtract() {},
+
+//   divide() {},
+// };
+
+/* 
+
+function Calculator(){
+  this.methods = {
+    "-" : (a,b) => a - b,
+    "+" : (a,b) => a + b
+  };
+
+  this.calculate = function (str){
+
+    let split = str.split(' ),
+      a = +split[0],
+      op = split[1],
+      b = +split[2];
+
+    if(!thismethod[op] || isNaN(a) || isNaN(b)){
+      return NaN;
+    }
+    return this.method[op](a,b);
+  }
+
+  this.addMethod = function (name, func){
+    this.method[name] = func
+  };
+}
+
+*/
