4065bc8697dc7525ff6eaff7ba5f8ef8625e6ed9
Rahul Mohan <88761343+rmohanmd@users.noreply.github.com>
2024-03-05 07:43:08 -0500

    Added calculator computation logic

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //Create a NodeList of all html elements with .button class
-let btns = document.querySelectorAll(".button");
-
+const btns = document.querySelectorAll(".button");
+const display = document.getElementById("display");
+let num1;
+let input = 0;
+let operator;
 //map through node list and add an eventListener that fires on click.
 // anonymous function so that the actual function with an argument (buttonPress) can be attached to the event listener
 btns.forEach((btn) =>
@@ -10,5 +13,36 @@ btns.forEach((btn) =>
 );
 
 function buttonPress(buttonValue) {
-  alert(`${buttonValue}`);
+  if (buttonValue == "=") {
+    switch (operator) {
+      case "+":
+        alert(num1 + Number(input));
+        break;
+      case "-":
+        alert(num1 - Number(input));
+        break;
+      case "X":
+        alert(num1 * Number(input));
+        break;
+      case "/":
+        alert(num1 / Number(input));
+        break;
+        num1 = 0;
+        input = "";
+        operator = "";
+    }
+  } else if (
+    buttonValue == "+" ||
+    buttonValue == "-" ||
+    buttonValue == "X" ||
+    buttonValue == "/"
+  ) {
+    operator = buttonValue;
+    num1 = Number(input);
+    input = "";
+    alert(num1);
+  } else {
+    input += buttonValue;
+    alert(input);
+  }
 }
