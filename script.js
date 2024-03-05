66278614a981474a18f97399ae88b7dbedc547d7
Rahul Mohan <88761343+rmohanmd@users.noreply.github.com>
2024-03-05 06:19:31 -0500

    Added logic to capture button input

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+//Create a NodeList of all html elements with .button class
+let btns = document.querySelectorAll(".button");
+
+//map through node list and add an eventListener that fires on click.
+// anonymous function so that the actual function with an argument (buttonPress) can be attached to the event listener
+btns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    buttonPress(btn.textContent);
+  })
+);
+
+function buttonPress(buttonValue) {
+  alert(`${buttonValue}`);
+}
