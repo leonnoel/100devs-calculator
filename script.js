a2c141d02780ef4e60942a946f684e4499627903
Rahul Mohan <88761343+rmohanmd@users.noreply.github.com>
2024-03-08 10:56:41 -0500

    Refactor and add functionality
    Code was refactored to seperate logic into functions
    
    Functionality was expanded to be able to calculate
    a) when users want to continue to calculate with result after pushing =.
    eg user enters 2+2 then pushes equal to get answer fo 4 and then want to continue calculation with the answer of 4
    b) users enter multiple calculations one after another without entering =
    eg 2+2+2+2

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 //Create a NodeList of all html elements with .button class
 const btns = document.querySelectorAll(".button");
 const display = document.getElementById("display");
-let num1;
+let storedNumber = null;
+let displayNumber = "";
 let operator = null;
 let operator2 = null;
 display.value = "";
+let equalEntered = false;
 //map through node list and add an eventListener that fires on click.
 // anonymous function so that the actual function with an argument (buttonPress) can be attached to the event listener
 btns.forEach((btn) =>
@@ -12,47 +14,73 @@ btns.forEach((btn) =>
     buttonPress(btn.textContent);
   })
 );
-function calculation() {
+//calculate function
+function computation() {
   switch (operator) {
     case "+":
-      display.value = num1 + Number(display.value);
+      display.value = +storedNumber + Number(displayNumber);
       break;
     case "-":
-      display.value = num1 - Number(display.value);
+      display.value = +storedNumber - Number(displayNumber);
       break;
     case "X":
-      display.value = num1 * Number(display.value);
+      display.value = +storedNumber * Number(displayNumber);
       break;
     case "/":
-      display.value = num1 / Number(display.value);
+      display.value = +storedNumber / Number(displayNumber);
       break;
   }
-  num1 = display.value;
-  operator = operator2;
+  storedNumber = display.value; //store the calculated number
+  operator = operator2; //in scenario where user is entering values and operators without pushing =
   operator2 = null;
+  displayNumber = ""; //resets the displayNumber variable
 }
+
+//evaluate when keypress is an operator
+function operatorEval(buttonValue) {
+  //check to make sure no number is stored from previous calculation
+  if (storedNumber == null) {
+    operator = buttonValue;
+    storedNumber = displayNumber;
+  }
+  //run if there is a stored number from previous calculation
+  else {
+    //scenario: previous calculation completed with equal
+    if (equalEntered) {
+      operator = buttonValue;
+      equalEntered = false; //reset flag for equal being pushed.
+    }
+    //scenario: previous calculation not completed because = wasn't entered.
+    //instead operator was entered again.
+    //must store operator2 and run computation with operator1
+    //eg user enters, 2+2+ (when second operator is pressed store it and must compute 2+2)
+    else {
+      operator2 = buttonValue;
+      computation();
+    }
+  }
+  displayNumber = "";
+}
+
+function displayScreen(buttonValue) {
+  display.value = "";
+  displayNumber += buttonValue;
+  display.value = displayNumber;
+}
+
+//evaluate button entered and call appropriate function
 function buttonPress(buttonValue) {
   if (buttonValue == "=") {
-    calculation();
+    equalEntered = true;
+    computation();
   } else if (
     buttonValue == "+" ||
     buttonValue == "-" ||
     buttonValue == "X" ||
     buttonValue == "/"
   ) {
-    if (operator == null) {
-      operator = buttonValue;
-      num1 = Number(display.value);
-      display.value = "";
-      //   isOperatorStored = true;
-    } else {
-      operator2 = buttonValue;
-      calculation();
-    }
+    operatorEval(buttonValue);
   } else {
-    if (operator != null) {
-      display.value = "";
-    }
-    display.value += buttonValue;
+    displayScreen(buttonValue);
   }
 }
