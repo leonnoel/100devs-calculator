b906ab0f2650b26d2ad54085925be25de26d8a29
tramirez30 <ramirez.tomas@yahoo.com>
2024-03-11 12:32:57 -0700

    Repaired issue, displayed two numbers while performing operation, now only displays one digit

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 class Calculator {
+  // Initializes constructor
   constructor(value1, value2) {
     this.value1 = value1;
     this.value2 = value2;
     this.clear();
   }
+
+  // Clears display
   clear() {
     this.currentDisplay = "";
     this.previousDisplay = "";
     this.operator = undefined;
   }
 
+  // Limits decimal points & checks whether a decimal exists
+  // Limits the length of the display to 9 digits
   appendValue(number) {
     if (number === "." && this.currentDisplay.includes(".")) return;
     if (this.currentDisplay.length > 9) return;
     this.currentDisplay = this.currentDisplay.toString() + number.toString();
   }
 
+  // Displays the number values on calculator
   updateDisplay() {
     this.value1.innerText = this.currentDisplay;
-    this.value2.innerText = this.previousDisplay;
+    // this.value2.innerText = this.previousDisplay;
   }
 
+  // Initializes operation once operand button is clicked
+  // Assigns operator value, updates previous value with current value
+  // Current Display is removed
   operation(operator) {
     if (this.currentDisplay === "") return;
     if (this.previousDisplay !== "") {
@@ -31,6 +40,8 @@ class Calculator {
     this.currentDisplay = "";
   }
 
+  // Stores 'total' in variable
+  // Stores current and previous value inside variables
   compute() {
     let total;
     const current = Number(this.currentDisplay);
@@ -40,34 +51,37 @@ class Calculator {
 
     switch (this.operator) {
       case "+":
-        total = current + previous;
+        total = previous + current;
         break;
       case "-":
-        total = current - previous;
+        total = previous - current;
         break;
       case "x":
-        total = current * previous;
+        total = previous * current;
         break;
       case "÷":
-        total = current / previous;
+        total = previous / current;
         break;
       default:
         break;
     }
+    this.previousDisplay = "";
     this.currentDisplay = total;
     this.operator = undefined;
-    this.previousDisplay = "";
   }
 }
 
+// HTML Elements
 let btnOperation = document.querySelectorAll("[data-operation]");
 let btnNumbers = document.querySelectorAll("[data-number]");
 let equalBtn = document.querySelector("[data-equal]");
 let currentValue = document.querySelector("#current-value");
 let previousValue = document.querySelector("#previous-value");
 
+// Creates new class 'calculator'
 const calculator = new Calculator(previousValue, currentValue);
 
+// Loops through calculator number buttons and adds event listener, calls calculator methods
 btnNumbers.forEach((button) => {
   button.addEventListener("click", function () {
     calculator.appendValue(button.innerText);
@@ -75,6 +89,7 @@ btnNumbers.forEach((button) => {
   });
 });
 
+// Loops through calculator operand buttons and adds event listener, calls calculator methods
 btnOperation.forEach((button) => {
   button.addEventListener("click", function () {
     calculator.operation(button.innerText);
@@ -82,6 +97,7 @@ btnOperation.forEach((button) => {
   });
 });
 
+// Adds event listener to equal button
 equalBtn.addEventListener("click", function () {
   calculator.compute();
   calculator.updateDisplay();
